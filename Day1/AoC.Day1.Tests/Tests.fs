4c5e621082e5c9742f4d86e3ee239fd32c74be14
Chris Simon <chris@chrissimon.au>
2024-12-02 08:08:41 +1100

    1: RED - test empty list

--- Day1/AoC.Day1.Tests/Tests.fs
+++ Day1/AoC.Day1.Tests/Tests.fs
@@ -1,8 +1,9 @@
-﻿module Tests
+﻿module AoC.Day1.Tests.Tests
 
 open System
 open Xunit
+open AoC.Day1.Distance
 
 [<Fact>]
-let ``My test`` () =
-    Assert.True(true)
+let ``Empty lists return 0 distance`` () =
+    Assert.Equal(0, distance [] [])
---
+++ Day1/AoC.Day1/Distance.fs
@@ -0,0 +1,3 @@
+module AoC.Day1.Distance
+
+let distance leftList rightList = raise <| System.NotImplementedException()
