28f3c90efbe47bc7fd747ebbddf28aad65551067
Chris Simon <chris@chrissimon.au>
2024-12-02 08:55:41 +1100

    8: REFACTOR - rename module in prep for part 2

--- Day1/AoC.Day1.Tests/Tests.fs
+++ Day1/AoC.Day1.Tests/Tests.fs
@@ -2,7 +2,7 @@
 
 open System
 open Xunit
-open AoC.Day1.Distance
+open AoC.Day1.ChiefHistorianFinding
 open TestData
 
 [<Fact>]
@@ -36,3 +36,40 @@ let ``Sample data from Advent of Code distance`` () =
 [<Fact>]
 let ``Test data from Advent of Code distance`` () =
     Assert.Equal(Test.distance, distance Test.left Test.right)
+
+
+[<Fact>]
+let ``Empty lists return 0 similarity`` () =
+    Assert.Equal(0, similarity [] [])
+
+// [<Fact>]
+// let ``Single element list with same values return 0 similarity`` () =
+//     Assert.Equal(0, similarity [3] [3])
+
+// [<Fact>]
+// let ``Single element list with diff values return similarity`` () =
+//     Assert.Equal(1, similarity [4] [3])
+
+// [<Fact>]
+// let ``Single element list with diff values in other order return similarity`` () =
+//     Assert.Equal(1, similarity [3] [4])
+
+// [<Fact>]
+// let ``Multi-element pre-sorted list similarity`` () =
+//     Assert.Equal(2, similarity [3; 4] [4; 5])
+
+// [<Fact>]
+// let ``Multi-element unsorted list similarity`` () =
+//     Assert.Equal(5, similarity [10; 4] [4; 5])
+
+// [<Fact>]
+// let ``Sample data from Advent of Code similarity`` () =
+//     Assert.Equal(Sample.similarity, similarity Sample.left Sample.right)
+
+// [<Fact>]
+// let ``Test data from Advent of Code similarity`` () =
+//     Assert.Equal(Test.similarity, similarity Test.left Test.right)
+
+
+
+
--- Day1/AoC.Day1/ChiefHistorianFinding.fs
+++ Day1/AoC.Day1/ChiefHistorianFinding.fs
@@ -1,7 +1,9 @@
-module AoC.Day1.Distance
+module AoC.Day1.ChiefHistorianFinding
 
 let distance (leftList: int list) (rightList: int list) = 
     (List.sort leftList)
     |> List.zip (List.sort rightList)
     |> List.map (fun (left, right) -> System.Math.Abs(left-right))
     |> List.sum
+
+let similarity (leftList: int list) (rightList: int list) = raise <| System.NotImplementedException()
