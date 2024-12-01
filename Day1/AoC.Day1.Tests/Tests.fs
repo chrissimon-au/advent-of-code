9cf9c7c8e003daa69526b363dc4c4e19eb7096dd
Chris Simon <chris@chrissimon.au>
2024-12-02 08:12:23 +1100

    4: RED - single element diff value out of order lists

--- Day1/AoC.Day1.Tests/Tests.fs
+++ Day1/AoC.Day1.Tests/Tests.fs
@@ -15,3 +15,7 @@ let ``Single element list with same values return 0 distance`` () =
 [<Fact>]
 let ``Single element list with diff values return distance`` () =
     Assert.Equal(1, distance [4] [3])
+
+[<Fact>]
+let ``Single element list with diff values in other order return distance`` () =
+    Assert.Equal(1, distance [3] [4])
