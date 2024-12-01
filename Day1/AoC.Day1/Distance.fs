f84075276434c05d42a23275619960d5f3882051
Chris Simon <chris@chrissimon.au>
2024-12-02 08:28:46 +1100

    6: GREEN - multi-element unsorted list

--- Day1/AoC.Day1/Distance.fs
+++ Day1/AoC.Day1/Distance.fs
@@ -1,7 +1,7 @@
 module AoC.Day1.Distance
 
 let rec distance (leftList: int list) (rightList: int list) = 
-    match leftList, rightList with
+    match List.sort leftList, List.sort rightList with
     | [], [] -> 0
     | [a], [b] -> System.Math.Abs(a-b)
     | (a :: aTail), (b :: bTail) -> System.Math.Abs(a-b) + (distance aTail bTail)
