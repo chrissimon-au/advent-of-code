b046eaf728b431428cce6f9e14b74e708c65d5da
Chris Simon <chris@chrissimon.au>
2024-12-02 08:11:58 +1100

    3: GREEN - single element diff value lists

--- Day1/AoC.Day1/Distance.fs
+++ Day1/AoC.Day1/Distance.fs
@@ -1,3 +1,7 @@
 module AoC.Day1.Distance
 
-let distance leftList rightList = 0
+let distance leftList rightList = 
+    match leftList, rightList with
+    | [], [] -> 0
+    | [a], [b] -> a-b
+    | _, _ -> raise <| System.NotImplementedException()
