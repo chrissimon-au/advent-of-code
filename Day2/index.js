52cea2f7b61b13b6ad1855c8942f73639ca207e6
Chris Simon <chris@chrissimon.au>
2024-12-02 16:31:09 +1100

    3: REFACTOR - make the parse function more readable

--- Day2/index.js
+++ Day2/index.js
@@ -1,7 +1,14 @@
 const { EOL } = require("os");
 
 function parse(input) {    
-    return input.split(EOL).map(row => row.split(/\s/).map(a => parseInt(a)));    
+    return input
+        .split(EOL)
+        .map(row => 
+            row
+            .split(/\s/)
+            .map(a => parseInt(a)
+        )
+    );
 }
 
 module.exports = {
