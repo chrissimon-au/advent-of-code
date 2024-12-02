c99a416049955d56f708c9f97edd047fcdcb1411
Chris Simon <chris@chrissimon.au>
2024-12-02 16:30:37 +1100

    3: GREEN - support multiple reports in input text

--- Day2/index.js
+++ Day2/index.js
@@ -1,5 +1,7 @@
+const { EOL } = require("os");
+
 function parse(input) {    
-    return [input.split(/\s/).map(a => parseInt(a))];
+    return input.split(EOL).map(row => row.split(/\s/).map(a => parseInt(a)));    
 }
 
 module.exports = {
