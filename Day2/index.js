13e1084ebeb8409d9cf8a019569bbb9fcd3eaefc
Chris Simon <chris@chrissimon.au>
2024-12-02 16:54:29 +1100

    8: GREEn - decreasing safe report

--- Day2/index.js
+++ Day2/index.js
@@ -15,11 +15,16 @@ function adjacentDifferences(report) {
     if (report.length < 2) {
         return [];
     }
-    return report.splice(1).map((value, index) => value - report[index]);
+    const diffs = report.splice(1).map((value, index) => value - report[index]);
+    if (diffs[0] < 0) {
+        return diffs.map (diff => diff * -1);
+    }
+    return diffs;
 }
 
 function isReportSafe(report) {
     const diffs = adjacentDifferences(report);
+
     return diffs.filter(diff => diff > 3 || diff < 1).length == 0;
 }
 
