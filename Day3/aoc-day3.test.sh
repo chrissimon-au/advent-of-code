23786315508a8455abbd934f331442185f0e7c6d
Chris Simon <chris@chrissimon.au>
2024-12-03 16:25:35 +1100

    2: RED - introduce a simple 1 x 1 case to test for arguments

--- Day3/aoc-day3.test.sh
+++ Day3/aoc-day3.test.sh
@@ -5,5 +5,10 @@ testEmptyMemory() {
   assertEquals "0" "${result}"
 }
 
+testMul1by1() {
+  result=`rockstar aoc-day3.rock "mul(1,1)"`
+  assertEquals "1" "${result}"
+}
+
 # Load shUnit2.
 . shunit2
