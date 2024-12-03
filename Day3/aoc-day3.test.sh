bca74005856997be501439adc752cd42e55070fe
Chris Simon <chris@chrissimon.au>
2024-12-03 19:55:58 +1100

    7: RED - ensure spaces mean it isn't computed

--- Day3/aoc-day3.test.sh
+++ Day3/aoc-day3.test.sh
@@ -35,5 +35,10 @@ testMul10by2WithCorruptedMemoryAfter() {
   assertEquals "20" "${result}"
 }
 
+testMulWithSpacesShouldDoNothing() {
+  result=`rockstar aoc-day3.rock "mul ( 2 , 4 )"`
+  assertEquals "0" "${result}"
+}
+
 # Load shUnit2.
 . shunit2
