3e0a6fcd6b9d248e1f6ad38b0323cc38bcfa7f28
Chris Simon <chris@chrissimon.au>
2024-12-04 17:05:30 +1100

    4: RED - reverse XMAS horizontally

--- Day04/aoc-day04/tests/test_day04.py
+++ Day04/aoc-day04/tests/test_day04.py
@@ -8,6 +8,7 @@ import pytest
     ("ASDFXMAS.. asdfXMAS234", 2),
     ("ASDFXMAS.. asdfXMAS234\n"
      "BLERG..XMAS...........", 3),
+    ("ASDFSAMX..", 1),
 ])
 def test_count_xmas(test_input: str, expected: str):
     assert count_xmas(test_input) == expected
