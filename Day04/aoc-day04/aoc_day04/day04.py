98f5b5b88dde11f29f8d35989bb7ce44eaf314f2
Chris Simon <chris@chrissimon.au>
2024-12-04 17:43:22 +1100

    6: GREEN - handle vertical XMAS with rotator function

--- Day04/aoc-day04/aoc_day04/day04.py
+++ Day04/aoc-day04/aoc_day04/day04.py
@@ -1,5 +1,20 @@
+import os
 XMAS = "XMAS"
 REVERSED_XMAS = XMAS[::-1]
 
+def rotate_90(input: str):
+    lines = input.split(os.linesep)
+    output = []
+    indexes = range(0,len(lines))
+    for i, c in enumerate(lines[0]):
+        new_line = [lines[r][i] for r in indexes]
+        output.append("".join(new_line))
+    return os.linesep.join(output)
+
 def count_xmas(input: str):
-    return input.count(XMAS) + input.count(REVERSED_XMAS)
+    all_lines = [
+        input,
+        rotate_90(input),
+    ]
+    all = os.linesep.join(all_lines)
+    return all.count(XMAS) + all.count(REVERSED_XMAS)
--- Day04/aoc-day04/tests/test_day04.py
+++ Day04/aoc-day04/tests/test_day04.py
@@ -15,5 +15,15 @@ import pytest
      "A\n"
      "S",1)
 ])
-def test_count_xmas(test_input: str, expected: str):
+def test_count_xmas(test_input: str, expected: int):
     assert count_xmas(test_input) == expected
+
+
+@pytest.mark.parametrize("test_input,expected", [
+    ("AB\n"
+     "CD",
+     "AC\n"
+     "BD")
+])
+def test_rotate(test_input: str, expected: str):
+    assert rotate_90(test_input) == expected
