b84f30ac8040d4f2b02f80cfd8dc972ea1d7031b
Chris Simon <chris@chrissimon.au>
2024-12-04 16:58:22 +1100

    4: GREEN - already handles multiple lines

--- Day04/aoc-day04/tests/test_day04.py
+++ Day04/aoc-day04/tests/test_day04.py
@@ -6,6 +6,8 @@ import pytest
     ("", 0),
     ("XMAS", 1),
     ("ASDFXMAS.. asdfXMAS234", 2),
+    ("ASDFXMAS.. asdfXMAS234\n"
+     "BLERG..XMAS...........", 3),
 ])
 def test_count_xmas(test_input: str, expected: str):
     assert count_xmas(test_input) == expected
