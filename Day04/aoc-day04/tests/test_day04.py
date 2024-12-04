6926605c196ba25f79936f9f5f6dbf8621027840
Chris Simon <chris@chrissimon.au>
2024-12-04 16:56:07 +1100

    2: RED - detect a single XMAS

--- Day04/aoc-day04/tests/test_day04.py
+++ Day04/aoc-day04/tests/test_day04.py
@@ -4,6 +4,7 @@ import pytest
 
 @pytest.mark.parametrize("test_input,expected", [
     ("", 0),
+    ("XMAS", 1),
 ])
 def test_count_xmas(test_input: str, expected: str):
     assert count_xmas(test_input) == expected
