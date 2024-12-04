d0892be1df907a084125033062842be50f80253b
Chris Simon <chris@chrissimon.au>
2024-12-04 16:56:41 +1100

    3: RED - test case of multiple XMAS on the same row

--- Day04/aoc-day04/tests/test_day04.py
+++ Day04/aoc-day04/tests/test_day04.py
@@ -5,6 +5,7 @@ import pytest
 @pytest.mark.parametrize("test_input,expected", [
     ("", 0),
     ("XMAS", 1),
+    ("ASDFXMAS.. asdfXMAS234", 2),
 ])
 def test_count_xmas(test_input: str, expected: str):
     assert count_xmas(test_input) == expected
