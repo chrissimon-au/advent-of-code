143470735f7c97e60325a8c6a03e3d76fc8d9f02
Chris Simon <chris@chrissimon.au>
2024-12-04 17:07:04 +1100

    4: REFACTOR - move xmas to constant

--- Day04/aoc-day04/aoc_day04/day04.py
+++ Day04/aoc-day04/aoc_day04/day04.py
@@ -1,2 +1,5 @@
+XMAS = "XMAS"
+REVERSED_XMAS = XMAS[::-1]
+
 def count_xmas(input: str):
-    return input.count("XMAS") + input.count("SAMX")
+    return input.count(XMAS) + input.count(REVERSED_XMAS)
