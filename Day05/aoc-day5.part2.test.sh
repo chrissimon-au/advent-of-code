b21e27216fa83e26bc2b265c8bafb72e80c13748
Chris Simon <chris@chrissimon.au>
2024-12-06 00:58:37 +1100

    12: GREEN - add AoC test data

--- Day05/aoc-day5.part2.test.sh
+++ Day05/aoc-day5.part2.test.sh
@@ -42,5 +42,12 @@ testSampleData() {
   assertEquals "${expected}" "${result}"
 }
 
+# testTestData() {
+#   input=`cat testdata.txt`
+#   expected=`cat testdata.part2.answer.txt`
+#   result=`day05 "${input}"`
+#   assertEquals "${expected}" "${result}"
+# }
+
 # Load shUnit2.
 . shunit2
