165f939da55f670411850c1137240a6339386441
Chris Simon <chris@chrissimon.au>
2024-12-06 16:37:52 +1100

    3: RED - add a blocker that forces movement

--- Day06/AoC-Day06/src/test/java/au/chrissimon/Day06Test.java
+++ Day06/AoC-Day06/src/test/java/au/chrissimon/Day06Test.java
@@ -8,7 +8,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class Day06Test {
 
     @ParameterizedTest()
-    @CsvSource({"^,1", ">.,2"})
+    @CsvSource({"^,1", ">.,2", ">.#,2"})
     public void test_GuardRoute(String map, int expectedVisitedLocationCount) {
         assertEquals(expectedVisitedLocationCount, Day06.getVisitedLocationCount(map));
     }
