d2c1c247535defe1ae9a88aeafebce5e37f6ae27
Chris Simon <chris@chrissimon.au>
2024-12-06 16:37:09 +1100

    2: RED - add test with multiple steps

--- Day06/AoC-Day06/src/test/java/au/chrissimon/Day06Test.java
+++ Day06/AoC-Day06/src/test/java/au/chrissimon/Day06Test.java
@@ -8,10 +8,9 @@ import static org.junit.jupiter.api.Assertions.*;
 class Day06Test {
 
     @ParameterizedTest()
-    @CsvSource({"^,1"})
+    @CsvSource({"^,1", ">.,2"})
     public void test_GuardRoute(String map, int expectedVisitedLocationCount) {
-        Day06 day06 = new Day06();
-        assertEquals(expectedVisitedLocationCount, day06.getVisitedLocationCount(map));
+        assertEquals(expectedVisitedLocationCount, Day06.getVisitedLocationCount(map));
     }
 
 }
