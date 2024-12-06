2716f9846dd049b64567ba6d973f4eb4f4766f3d
Chris Simon <chris@chrissimon.au>
2024-12-06 20:10:40 +1100

    4: RED - add multi-line maps

--- Day06/AoC-Day06/src/test/java/au/chrissimon/Day06Test.java
+++ Day06/AoC-Day06/src/test/java/au/chrissimon/Day06Test.java
@@ -1,14 +1,33 @@
 package au.chrissimon;
 
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class Day06Test {
 
+    static final String multiLineMap1 = """
+        .#..
+        .>.#
+        ....
+        ..#.
+        """;
+
+    private static Stream<Arguments> testMaps() {
+        return Stream.of(
+                Arguments.of("^", 1),
+                Arguments.of(">.", 2),
+                Arguments.of(">.#", 2),
+                Arguments.of(multiLineMap1, 5)
+        );
+    }
+
     @ParameterizedTest()
-    @CsvSource({"^,1", ">.,2", ">.#,2"})
+    @MethodSource("testMaps")
     public void test_GuardRoute(String map, int expectedVisitedLocationCount) {
         assertEquals(expectedVisitedLocationCount, Day06.getVisitedLocationCount(map));
     }
