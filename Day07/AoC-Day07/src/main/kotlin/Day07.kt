d4562ed75249ef8ecc02d24a3742c7727dad7de7
Chris Simon <chris@devcycles.io>
2024-12-07 20:16:46 +1100

    5: GREEN - compute possible totals for different operations with max 2 operands

--- Day07/AoC-Day07/src/main/kotlin/Day07.kt
+++ Day07/AoC-Day07/src/main/kotlin/Day07.kt
@@ -1,9 +1,16 @@
 package au.chrissimon
 
+typealias Operation = (Int, Int) -> Int
+
+val operations: List<Operation> = listOf(
+    { a, b -> a + b },
+    { a, b -> a * b }
+)
+
 fun totalValidCalibrationResult(equations: String): Int {
     val equationParts = equations.split(":")
     val total = equationParts[0].toInt()
     val operands = equationParts[1].trim().split(" ").map {it.toInt()}
-    val calculatedTotal = operands.sumOf {it}
-    return if (total == calculatedTotal) { total } else { 0 }
+    val calculatedTotals = operations.map { operands.reduce(it) }
+    return if (calculatedTotals.any { it == total }) { total } else { 0 }
 }
