ddf787e9daeb9ff72e94e2be2aaaf2b8b310d3c8
Chris Simon <chris@chrissimon.au>
2024-12-07 21:12:17 +1100

    8: GREEN - iterate list of equations and only sum the valid ones

--- Day07/AoC-Day07/src/main/kotlin/Day07.kt
+++ Day07/AoC-Day07/src/main/kotlin/Day07.kt
@@ -9,22 +9,17 @@ val operations: List<Operation> = listOf(
     { a, b -> a * b }
 )
 
-private fun getOperationSets(numOps: Int): List<List<Operation>> {
-    val numCombos = 2.0.pow(numOps).toInt()-1
-    return (1..numCombos+1).map { i ->
-        (0..numOps).map { j ->
-            val bitPos = 2.0.pow(j).toInt()
-            if ((i and bitPos) > 0) {
-                operations[1]
-            } else {
-                operations[0]
-            }
+infix fun Int.pow(exponent: Int): Int = toDouble().pow(exponent).toInt()
+
+private fun getOperationSets(numOps: Int): List<List<Operation>> =
+    (1..(2 pow numOps)).map { i ->
+        (0..numOps).map {
+            operations[((i and (2 pow it)) > 0).compareTo(false)]
         }
     }
-}
 
-fun totalValidCalibrationResult(equations: String): Int {
-    val equationParts = equations.split(":")
+fun totalValidCalibrationForEquation(equation: String): Int {
+    val equationParts = equation.split(":")
     val total = equationParts[0].toInt()
     val operands = equationParts[1].trim().split(" ").map {it.toInt()}
     val operationSets = getOperationSets(operands.size - 1)
@@ -37,5 +32,12 @@ fun totalValidCalibrationResult(equations: String): Int {
     } else {
         listOf(operands[0])
     }
-    return if (calculatedTotals.any { it == total }) { total } else { 0 }
+    return if (calculatedTotals.any { it == total }) {
+        total
+    } else {
+        0
+    }
 }
+
+fun totalValidCalibrationResult(equations: String): Int =
+    equations.split(System.lineSeparator()).sumOf { totalValidCalibrationForEquation(it) }
