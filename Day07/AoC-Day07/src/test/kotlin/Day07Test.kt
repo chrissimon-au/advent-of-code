c14449a5093717f7029bbcfbc6c3e98910ebcfb5
Chris Simon <chris@chrissimon.au>
2024-12-07 19:34:18 +1100

    2: RED - add a second simple equation with a different result

--- Day07/AoC-Day07/src/test/kotlin/Day07Test.kt
+++ Day07/AoC-Day07/src/test/kotlin/Day07Test.kt
@@ -1,11 +1,15 @@
 import au.chrissimon.Day07
 import org.junit.jupiter.api.Assertions.*
-import kotlin.test.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+
 
 class Day07Test {
-    @Test
-    fun testCheck() {
-        val day07 = Day07("1: 1")
-        assertEquals(1, day07.totalValidCalibrationResult())
+
+    @ParameterizedTest
+    @CsvSource("1: 1, 1", "2: 2, 2")
+    fun testCheck(equations: String, expectedResult: Int) {
+        val day07 = Day07(equations)
+        assertEquals(expectedResult, day07.totalValidCalibrationResult())
     }
 }
