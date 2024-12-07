e2579a8276a5658a7a20d607d05f897c1e1314b4
Chris Simon <chris@chrissimon.au>
2024-12-07 20:55:10 +1100

    6: REFACTOR - move tests to use method source to simplify multi-line

--- Day07/AoC-Day07/src/test/kotlin/Day07Test.kt
+++ Day07/AoC-Day07/src/test/kotlin/Day07Test.kt
@@ -1,13 +1,27 @@
 import au.chrissimon.totalValidCalibrationResult
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.CsvSource
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 
 class Day07Test {
+    companion object {
+        @JvmStatic
+        fun testEquations(): List<Arguments> {
+            return listOf(
+                Arguments.of("1: 1", 1),
+                Arguments.of("2: 2", 2),
+                Arguments.of("3: 1", 0),
+                Arguments.of("2: 1 1", 2),
+                Arguments.of("6: 3 2", 6),
+                Arguments.of("17: 5 3 2", 17)
+            )
+        }
+    }
 
-    @ParameterizedTest
-    @CsvSource("1: 1, 1", "2: 2, 2", "3: 1, 0", "2: 1 1, 2", "6: 3 2, 6", "17: 5 3 2, 17")
+    @ParameterizedTest()
+    @MethodSource("testEquations")
     fun testCheck(equations: String, expectedResult: Int) {
         assertEquals(expectedResult, totalValidCalibrationResult(equations))
     }
