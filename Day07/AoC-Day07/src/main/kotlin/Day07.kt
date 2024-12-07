17332008adb0973d05c4f0c83c4fd7d05b335f4a
Chris Simon <chris@devcycles.io>
2024-12-07 20:00:45 +1100

    4: RED - actual equation

--- Day07/AoC-Day07/src/main/kotlin/Day07.kt
+++ Day07/AoC-Day07/src/main/kotlin/Day07.kt
@@ -1,11 +1,8 @@
 package au.chrissimon
 
-class Day07(val equations: String) {
-
-    fun totalValidCalibrationResult(): Int {
-        val equationParts = equations.split(":")
-        val total = equationParts[0].toInt()
-        val equation = equationParts[1].trim().toInt();
-        return if (total == equation) { total } else { 0 }
-        }
+fun totalValidCalibrationResult(equations: String): Int {
+    val equationParts = equations.split(":")
+    val total = equationParts[0].toInt()
+    val equation = equationParts[1].trim().toInt();
+    return if (total == equation) { total } else { 0 }
 }
--- Day07/AoC-Day07/src/test/kotlin/Day07Test.kt
+++ Day07/AoC-Day07/src/test/kotlin/Day07Test.kt
@@ -1,4 +1,4 @@
-import au.chrissimon.Day07
+import au.chrissimon.totalValidCalibrationResult
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
@@ -7,9 +7,8 @@ import org.junit.jupiter.params.provider.CsvSource
 class Day07Test {
 
     @ParameterizedTest
-    @CsvSource("1: 1, 1", "2: 2, 2", "3: 1, 0")
+    @CsvSource("1: 1, 1", "2: 2, 2", "3: 1, 0", "2: 1 1, 2")
     fun testCheck(equations: String, expectedResult: Int) {
-        val day07 = Day07(equations)
-        assertEquals(expectedResult, day07.totalValidCalibrationResult())
+        assertEquals(expectedResult, totalValidCalibrationResult(equations))
     }
 }
