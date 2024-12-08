4640e4ac9bcc15ff5b1c746123b5ba484e99a605
Chris Simon <chris@devcycles.io>
2024-12-08 18:55:17 +1100

    7: GREEN - use map of freqency to node sets

--- Day08/aoc-day08/src/main/scala/Main.scala
+++ Day08/aoc-day08/src/main/scala/Main.scala
@@ -1,13 +1,20 @@
-case class Location(val col: Int, val frequency: Char) {
+import scala.collection.immutable.HashMap
+case class Location(val col: Int) {
 }
+type Frequency = Char
+case class Antenna(val location: Location, val frequency: Frequency)
+
+type Frequencies = Map[Frequency, List[Location]]
 
 type Pair = List[Location]
 
-def getNodes(grid: String): List[Location] =
+def getNodes(grid: String): Frequencies =
   grid
-    .zipWithIndex.map((c, i) => Location(i, c))
+    .zipWithIndex.map((c, i) => Antenna(Location(i), c))
     .filter(_.frequency != '.')
     .toList
+    .groupMap(_.frequency)(_.location)
+
 
 def getPairs(nodes: List[Location]): List[Pair] = 
   nodes.toSet.subsets(2).map(_.toList.sortBy(_.col)).toList
@@ -16,11 +23,14 @@ def getAntiNodes(pairs: List[Pair]): List[Location] =
   pairs.flatMap((p) => {
     var delta = (p(0).col - p(1).col).abs
     List(
-      Location(p(0).col - delta, p(0).frequency),
-      Location(p(1).col + delta, p(1).frequency)
+      Location(p(0).col - delta),
+      Location(p(1).col + delta)
     )
   })
 
 @main def countAntiNodeLocation(grid: String): Int =
-  getAntiNodes(getPairs(getNodes(grid))).toSet.size
+  val nodes = getNodes(grid)
+  val pairs = nodes.map((frequency, locations) => (frequency, getPairs(locations)))
+  val antiNodes = pairs.flatMap((frequency, pairs) => getAntiNodes(pairs))
+  antiNodes.toSet.size
   
