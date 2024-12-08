575dee3c3af1ec42586ccad40ebd3ed86700087c
Chris Simon <chris@chrissimon.au>
2024-12-08 20:14:37 +1100

    10: REFACTOR - prep for part 2

--- Day08/aoc-day08/src/main/scala/Main.scala
+++ Day08/aoc-day08/src/main/scala/Main.scala
@@ -1,47 +1,67 @@
-case class Location(val col: Int, val row: Int) {
-  def -(that: Location): Location =
-    new Location(this.col - that.col, this.row - that.row)
-  def +(that: Location): Location =
-    new Location(this.col + that.col, this.row + that.row)
+case class Vector(val col: Int, val row: Int) {
+  def -(that: Vector): Vector =
+    new Vector(this.col - that.col, this.row - that.row)
+  def +(that: Vector): Vector =
+    new Vector(this.col + that.col, this.row + that.row)
+  def >=(that: Vector): Boolean =
+    this.col >= that.col && this.row >= that.row
+  def <(that: Vector): Boolean = 
+    this.col < that.col && this.row < that.row
 }
+
+def Origin = new Vector(0,0)  
+
 type Frequency = Char
-case class Antenna(val location: Location, val frequency: Frequency)
 
-type Frequencies = Map[Frequency, List[Location]]
+case class Antenna(val location: Vector, val frequency: Frequency)
 
-type Pair = List[Location]
+class AntennaMap(val size: Vector, val pairs: Map[Frequency, List[Pair]]) {
+  
+  def getAntiNodes(antiNodeAlgorithm: (List[Pair], Vector) => List[Vector]): Set[Vector] =
+    pairs.flatMap((frequency, pairs) => antiNodeAlgorithm(pairs, size)).toSet
+  
+}
 
-def getNodes(grid: String): (Frequencies, Location) =
-  val gridMap = grid
-    .split(System.lineSeparator())
-  (gridMap.zipWithIndex.map((row, rowIdx) => {
-    row
-      .zipWithIndex.map((c, colIdx) => Antenna(Location(colIdx, rowIdx), c))
-      .filter(_.frequency != '.')
-      .toList
-  }).flatMap(a => a).toList
-  .groupMap(_.frequency)(_.location),Location(gridMap(0).size,gridMap.size))
+object AntennaMap {
 
-def getPairs(nodes: List[Location]): List[Pair] = 
+  def antennas(map: Array[String]): Frequencies =
+    map.zipWithIndex.map((row, rowIdx) => {
+      row
+        .zipWithIndex.map((c, colIdx) => Antenna(Vector(colIdx, rowIdx), c))
+        .filter(_.frequency != '.')
+        .toList
+    })
+      .flatMap(a => a).toList
+      .groupMap(_.frequency)(_.location)
+
+  def pairs(antennas: Frequencies): Map[Frequency, List[Pair]] = 
+    antennas.map((frequency, locations) => (frequency, getPairs(locations)))
+
+  def apply(input: String): AntennaMap =
+    val map = input
+      .split(System.lineSeparator())
+    new AntennaMap(Vector(map(0).size,map.size), pairs(antennas(map)))
+}
+
+type Frequencies = Map[Frequency, List[Vector]]
+
+type Pair = List[Vector]
+
+def getPairs(nodes: List[Vector]): List[Pair] = 
   nodes.toSet.subsets(2).map(_.toList).toList
 
-def getAntiNodes(pairs: List[Pair], gridSize: Location): List[Location] =
+def getPart1AntiNodes(pairs: List[Pair], gridSize: Vector): List[Vector] =
   pairs.flatMap((p) => {
     var delta = p(1) - p(0)
     List(
       p(0) - delta,
       p(1) + delta
     )
-  }).filter(l => l.col >= 0 && l.col < gridSize.col && l.row >= 0 && l.row < gridSize.row)
-
-@main def countAntiNodeLocation(grid: String): Int =
-  val (nodes, gridSize) = getNodes(grid)
-  val pairs = nodes.map((frequency, locations) => (frequency, getPairs(locations)))
-  println(grid)
-  println(gridSize)
-  pairs.foreach(println)
-  val antiNodes = pairs.flatMap((frequency, pairs) => getAntiNodes(pairs, gridSize))
-  println("---")
-  antiNodes.foreach(println)
-  antiNodes.toSet.size
+  }).filter(l => l >= Origin && l < gridSize)
   
+
+@main
+def countAntiNodeLocation(input: String): Int =
+  val antennaMap = AntennaMap(input)
+  val antiNodes = antennaMap.getAntiNodes(getPart1AntiNodes)
+  antiNodes.size
