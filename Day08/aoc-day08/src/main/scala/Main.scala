a253dfc7c71542c590a8be4f86fa3153764a6e65
Chris Simon <chris@devcycles.io>
2024-12-08 16:41:17 +1100

    3: GREEN - flat case where the number of anti-nodes is the number of nodes

--- Day08/aoc-day08/src/main/scala/Main.scala
+++ Day08/aoc-day08/src/main/scala/Main.scala
@@ -1,2 +1,13 @@
+class Location(var col: Int, var frequency: Char) {
+}
+
+def getNodes(grid: String): List[Location] =
+  grid
+    .zipWithIndex.map((c, i) => Location(i, c))
+    .filter(_.frequency != '.')
+    .toList
+
+
 @main def countAntiNodeLocation(grid: String): Int =
-  0
+  var nodes = getNodes(grid)
+  nodes.size
