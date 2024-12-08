96ba880ef9416ba6a25b40430c5ac35171381a52
Chris Simon <chris@devcycles.io>
2024-12-08 16:52:36 +1100

    5: GREEN - use factorial functional to compute count

--- Day08/aoc-day08/src/main/scala/Main.scala
+++ Day08/aoc-day08/src/main/scala/Main.scala
@@ -1,6 +1,15 @@
 class Location(var col: Int, var frequency: Char) {
 }
 
+def factorial(n: Int): Int = {  
+    var f = 1
+    for(i <- 1 to n)
+    {
+        f = f * i;
+    }
+    return f
+}
+
 def getNodes(grid: String): List[Location] =
   grid
     .zipWithIndex.map((c, i) => Location(i, c))
@@ -10,4 +19,6 @@ def getNodes(grid: String): List[Location] =
 
 @main def countAntiNodeLocation(grid: String): Int =
   var nodes = getNodes(grid)
-  nodes.size
+  var n = nodes.size
+  var divisor = if (n-2 > 0) factorial(n-2) else 1
+  factorial(n) / (factorial(2) * divisor) * 2
