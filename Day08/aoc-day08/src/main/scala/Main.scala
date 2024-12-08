86bf6da60e9de2da9de2f3524e34c61a0141ba75
Chris Simon <chris@devcycles.io>
2024-12-08 19:24:52 +1100

    8: GREEN - compute locations from col and row coords

--- Day08/aoc-day08/src/main/scala/Main.scala
+++ Day08/aoc-day08/src/main/scala/Main.scala
@@ -1,6 +1,4 @@
-import scala.collection.immutable.HashMap
-case class Location(val col: Int) {
-}
+case class Location(val col: Int, val row: Int) {}
 type Frequency = Char
 case class Antenna(val location: Location, val frequency: Frequency)
 
@@ -9,28 +7,35 @@ type Frequencies = Map[Frequency, List[Location]]
 type Pair = List[Location]
 
 def getNodes(grid: String): Frequencies =
-  grid
-    .zipWithIndex.map((c, i) => Antenna(Location(i), c))
-    .filter(_.frequency != '.')
-    .toList
-    .groupMap(_.frequency)(_.location)
-
+  val gridMap = grid
+    .split(System.lineSeparator())
+  gridMap.zipWithIndex.map((row, rowIdx) => {
+    row
+      .zipWithIndex.map((c, colIdx) => Antenna(Location(colIdx, rowIdx), c))
+      .filter(_.frequency != '.')
+      .toList
+  }).flatMap(a => a).toList
+  .groupMap(_.frequency)(_.location)
 
 def getPairs(nodes: List[Location]): List[Pair] = 
-  nodes.toSet.subsets(2).map(_.toList.sortBy(_.col)).toList
+  nodes.toSet.subsets(2).map(_.toList.sortBy(l => l.col * l.row)).toList
 
 def getAntiNodes(pairs: List[Pair]): List[Location] =
   pairs.flatMap((p) => {
-    var delta = (p(0).col - p(1).col).abs
+    var delta = Location((p(0).col - p(1).col).abs, (p(0).row - p(1).row).abs)
     List(
-      Location(p(0).col - delta),
-      Location(p(1).col + delta)
+      Location(p(0).col - delta.col, p(0).row - delta.row),
+      Location(p(1).col + delta.col, p(1).row + delta.row)
     )
   })
 
 @main def countAntiNodeLocation(grid: String): Int =
   val nodes = getNodes(grid)
   val pairs = nodes.map((frequency, locations) => (frequency, getPairs(locations)))
+  println(grid)
+  pairs.foreach(println)
   val antiNodes = pairs.flatMap((frequency, pairs) => getAntiNodes(pairs))
+  println("---")
+  antiNodes.foreach(println)
   antiNodes.toSet.size
   
