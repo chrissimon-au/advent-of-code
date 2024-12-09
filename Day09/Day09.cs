c668c089e4bf0b533c5ced8b4232c4e538ab0cac
Chris Simon <chris@devcycles.io>
2024-12-09 16:24:04 +1100

    1: GREEN - Initial simple test with a 0 checksum

--- Day09/Day09.cs
+++ Day09/Day09.cs
@@ -2,5 +2,5 @@ namespace AoC.Day09;
 
 public static class Day09
 {
-    public static long CompactAndGetChecksum(string diskMap) => -1;
+    public static long CompactAndGetChecksum(string diskMap) => 0;
 }
