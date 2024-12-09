1959110d26e029a64e22f078726e85b02c0375ab
Chris Simon <chris@devcycles.io>
2024-12-09 19:07:50 +1100

    9: GREEN - Map char to file Id earlier so we track fileIds > 0

--- Day09/Day09.cs
+++ Day09/Day09.cs
@@ -7,32 +7,32 @@ public static class Day09
 {
     private static String NormalizeLength(this String input) => input.Length % 2 == 1 ? input + "0" : input;
     
-    private static IEnumerable<char> ExpandDiskMap(string diskMap) {
+    private static IEnumerable<long> ExpandDiskMap(string diskMap) {
         var fileAllocation = diskMap
             .Where((_, i) => i % 2 == 0)
-            .Select((fileSize, fileId) => Enumerable.Repeat(fileId.ToString()[0], fileSize-'0'));
+            .Select((fileSize, fileId) => Enumerable.Repeat((long)fileId, fileSize-'0'));
         var spaceAllocation = diskMap
             .Where((_, i) => i % 2 == 1)
-            .Select((spaceSize) => Enumerable.Repeat('.', spaceSize - '0'));
+            .Select((spaceSize) => Enumerable.Repeat((long)-1, spaceSize - '0'));
         var expandedLayout = fileAllocation.Zip(spaceAllocation).SelectMany((entry) => entry.First.Concat(entry.Second));        
         return expandedLayout;
     }
 
-    private static IEnumerable<char> CompressDiskMap(this IEnumerable<char> input)
+    private static IEnumerable<long> CompressDiskMap(this IEnumerable<long> input)
     {
         var diskMap = input.ToList();
         var len = diskMap.Count;
         var compressedBlocks = 0;
-        var compressedDiskMap = diskMap.Select((c, i) =>
+        var compressedDiskMap = diskMap.Select((fileId, idx) =>
         {
-            if (i >= len - compressedBlocks)
+            if (idx >= len - compressedBlocks)
             {
-                return '.';
+                return -1;
             }
-            if (c == '.')
+            if (fileId < 0)
             {
                 compressedBlocks++;
-                while (diskMap[len - compressedBlocks] == '.')
+                while (diskMap[len - compressedBlocks] < 0)
                 {
                     compressedBlocks++;
                 }
@@ -40,17 +40,17 @@ public static class Day09
             }
             else
             {
-                return c;
+                return fileId;
             }
-        });
-        return compressedDiskMap;//.Take(len - compressedBlocks);
+        }).ToList();
+        return compressedDiskMap;
     }
     
     public static long CompactAndGetChecksum(string diskMap) => 
         ExpandDiskMap(diskMap.NormalizeLength())
             .CompressDiskMap()
-            .Where(c => c != '.')
-            .Select((fileNumber, idx) => idx * (fileNumber - '0'))
+            .Where(c => c >= 0)
+            .Select((fileNumber, idx) => idx * fileNumber)
             .Sum();
     
 }
