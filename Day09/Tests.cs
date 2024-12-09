e05d3a88233d7be7cdcd36a2007bbc0077ce4f8c
Chris Simon <chris@devcycles.io>
2024-12-09 16:32:59 +1100

    2: RED - add case with 2 disk blocks and no compaction

--- Day09/Tests.cs
+++ Day09/Tests.cs
@@ -4,6 +4,7 @@ public class Tests
 {
     [Theory()]
     [InlineData("1", 0)]
+    [InlineData("101", 1)]
     public void test_FileCompaction(string diskMap, long expectedChecksum)
     {
         Assert.Equal(expectedChecksum, Day09.CompactAndGetChecksum(diskMap));
