877adbbf7be78a24c75d45dd610ee63c14d2609c
Chris Simon <chris@devcycles.io>
2024-12-09 17:45:39 +1100

    7: RED - add scenario with compaction more than is available

--- Day09/Tests.cs
+++ Day09/Tests.cs
@@ -7,6 +7,7 @@ public class Tests
     [InlineData("101", 1)]
     [InlineData("10101", 5)]
     [InlineData("11101", 4)]
+    [InlineData("131221", 9)]
     public void test_FileCompaction(string diskMap, long expectedChecksum)
     {
         Assert.Equal(expectedChecksum, Day09.CompactAndGetChecksum(diskMap));
