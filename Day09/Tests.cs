ba972bd9a3a84f98c525fe32ec460ce2b52b4612
Chris Simon <chris@devcycles.io>
2024-12-09 16:37:46 +1100

    3: RED - add extra case with no compaction to test higher file numbers

--- Day09/Tests.cs
+++ Day09/Tests.cs
@@ -5,6 +5,7 @@ public class Tests
     [Theory()]
     [InlineData("1", 0)]
     [InlineData("101", 1)]
+    [InlineData("10101", 5)]
     public void test_FileCompaction(string diskMap, long expectedChecksum)
     {
         Assert.Equal(expectedChecksum, Day09.CompactAndGetChecksum(diskMap));
