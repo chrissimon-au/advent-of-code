415a40909a99668befd30faf1cb18f1ef8107560
Chris Simon <chris@devcycles.io>
2024-12-09 17:31:11 +1100

    5: GREEN - add compaction algorithm

--- Day09/Day09.cs
+++ Day09/Day09.cs
@@ -4,13 +4,37 @@ using System.Linq;
 public static class Day09
 {
     private static IEnumerable<char> ExpandDiskMap(string diskMap) {
+        if (diskMap.Length % 2 == 1)
+        {
+            diskMap += "0";
+        }
         var files = diskMap.Where((_, i) => i % 2 == 0);
-        return files;
+        var fileAllocation = files.Select((fileSize, fileId) => Enumerable.Repeat(fileId.ToString()[0], fileSize-'0'));
+        var spaces = diskMap.Where((_, i) => i % 2 == 1);
+        var spaceAllocation = spaces.Select((spaceSize) => Enumerable.Repeat('.', spaceSize - '0'));
+        var expanededLayout = fileAllocation.Zip(spaceAllocation).SelectMany((entry) => entry.First.Concat(entry.Second));        
+        return expanededLayout;
     }
     public static long CompactAndGetChecksum(string diskMap)
     {
-        var expandedDiskMap = ExpandDiskMap(diskMap);
-        var fileNumbers = expandedDiskMap.Select((c, i) => i);
-        return fileNumbers.Select((fileNumber, idx) => idx * fileNumber).Sum();
+        var expandedDiskMap = ExpandDiskMap(diskMap).ToList();
+        var len = expandedDiskMap.Count;
+        var compressedBlocks = 0;
+        var compressedDiskMap = expandedDiskMap.Select((c, i) =>
+        {
+            if (c == '.')
+            {
+                compressedBlocks++;
+                return expandedDiskMap[len - compressedBlocks];
+            }
+            else
+            {
+                return c;
+            }
+        }).ToList();
+        var activeBlocks = compressedDiskMap.Take(len - compressedBlocks);
+        return activeBlocks.Where(c => c != '.')
+            .Select((fileNumber, idx) => idx * (fileNumber - '0'))
+            .Sum();
     }
 }
