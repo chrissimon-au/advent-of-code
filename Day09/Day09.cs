c04ee3472252b9fa87dc09702e48f9179605862c
Chris Simon <chris@devcycles.io>
2024-12-09 16:37:35 +1100

    2: GREEN - add case with 2 disk blocks and no compaction

--- Day09/Day09.cs
+++ Day09/Day09.cs
@@ -1,6 +1,15 @@
 namespace AoC.Day09;
+using System.Linq;
 
 public static class Day09
 {
-    public static long CompactAndGetChecksum(string diskMap) => 0;
+    private static IEnumerable<char> ExpandDiskMap(string diskMap) {
+        var files = diskMap.Where((_, i) => i % 2 == 0);
+        return files;
+    }
+    public static long CompactAndGetChecksum(string diskMap)
+    {
+        var expandedDiskMap = ExpandDiskMap(diskMap);
+        return expandedDiskMap.Select((c, i) => (c - '0') * i).Sum();
+    }
 }
