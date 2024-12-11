ffd1e99c738b626da2f6a288686c10c29119201b
Chris Simon <chris@chrissimon.au>
2024-12-11 21:42:30 +1100

    6. GREEN - experimental performance optimisation

--- Day11/AoC-Day11/src/Lib.hs
+++ Day11/AoC-Day11/src/Lib.hs
@@ -7,14 +7,64 @@ module Lib
 blinkAtStone :: Int -> [Int]
 blinkAtStone 0 = [1]
 blinkAtStone stone
-  | mod lenStoneStr 2 == 0 = [read (take (div lenStoneStr 2) stoneStr), read (drop (div lenStoneStr 2) stoneStr)]
+  | mod lenStoneStr 2 == 0 = [read (take halfLen stoneStr), read (drop halfLen stoneStr)]
   | otherwise = [stone * 2024]
   where stoneStr = show stone
         lenStoneStr = length(stoneStr)
+        halfLen = div lenStoneStr 2
   
 
 blinkAtList :: [Int] -> [Int]
 blinkAtList stones = stones >>= blinkAtStone
 
+
+numOfStones :: Int -> Int -> Int
+numOfStones 0 _ = 1
+numOfStones 3 stone | stone >= 1 && stone <= 4 = 4
+numOfStones 2 stone | stone >= 1 && stone <= 4 = 2
+numOfStones 1 stone | stone >= 1 && stone <= 4 = 1
+
+numOfStones 5 stone | stone >= 5 && stone <= 9 && stone /= 8 = 8
+numOfStones 5 8 = 7
+numOfStones 4 stone | stone == 0 || (stone >= 5 && stone <= 9) = 4
+numOfStones 3 stone | stone == 0 || (stone >= 5 && stone <= 9) = 2
+numOfStones 2 stone | stone == 0 || (stone >= 5 && stone <= 9) = 1
+numOfStones 1 stone | stone == 0 || (stone >= 5 && stone <= 9) = 1
+
+numOfStones numOfBlinks 0
+  | numOfBlinks >= 4 = numberOfStonesAfter [2, 0, 2, 4] (numOfBlinks-4)
+
+numOfStones numOfBlinks 1
+  | numOfBlinks >= 3 = numberOfStonesAfter [2, 0, 2, 4] (numOfBlinks-3)
+
+numOfStones numOfBlinks 2
+  | numOfBlinks >= 3 = numberOfStonesAfter [4, 0, 4, 8] (numOfBlinks-3)
+
+numOfStones numOfBlinks 3
+  | numOfBlinks >= 3 = numberOfStonesAfter [6, 0, 7, 2] (numOfBlinks-3)
+
+numOfStones numOfBlinks 4
+  | numOfBlinks >= 3 = numberOfStonesAfter [8, 0, 9, 6] (numOfBlinks-3)
+
+numOfStones numOfBlinks 5
+  | numOfBlinks >= 5 = numberOfStonesAfter [2, 0, 4, 8, 2, 8, 8, 0] (numOfBlinks-5)
+
+numOfStones numOfBlinks 6
+  | numOfBlinks >= 5 = numberOfStonesAfter [2, 4, 5, 7, 9, 4, 5, 6] (numOfBlinks-5)
+
+numOfStones numOfBlinks 7
+  | numOfBlinks >= 5 = numberOfStonesAfter [2, 8, 6, 7, 6, 0, 3, 2] (numOfBlinks-5)
+
+numOfStones numOfBlinks 8
+  | numOfBlinks >= 5 = (numOfStones (numOfBlinks-4) 8) + (numberOfStonesAfter [3, 2, 7, 7, 2, 6] (numOfBlinks-5))
+
+numOfStones numOfBlinks 9
+  | numOfBlinks >= 5 = numberOfStonesAfter [3, 6, 8, 6, 9, 1, 8, 4] (numOfBlinks-5)
+
+numOfStones numOfBlinks stone = numberOfStonesAfter (blinkAtStone stone) (numOfBlinks-1)
+
+
 numberOfStonesAfter :: [Int] -> Int -> Int
-numberOfStonesAfter stones numOfBlinks = length(iterate blinkAtList stones !! numOfBlinks)
+numberOfStonesAfter stones 0 = length(stones)
+numberOfStonesAfter stones numOfBlinks = sum(map (numOfStones numOfBlinks) stones)
+--numberOfStonesAfter stones numOfBlinks = length(iterate blinkAtList stones !! numOfBlinks)
