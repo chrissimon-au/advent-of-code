848d5bffd1d5411c2224e91c8ec34cd31e597dcc
Chris Simon <chris@chrissimon.au>
2024-12-11 16:38:03 +1100

    3. GREEN - handle even length num of digits

--- Day11/AoC-Day11/src/Lib.hs
+++ Day11/AoC-Day11/src/Lib.hs
@@ -1,7 +1,19 @@
 module Lib
     ( 
-      numberOfStonesAfter
+      numberOfStonesAfter,
+      blinkAtList
     ) where
 
+blinkAtStone :: Int -> [Int]
+blinkAtStone 0 = [1]
+blinkAtStone stone
+  | mod lenStoneStr 2 == 0 = [read (take (div lenStoneStr 2) stoneStr), read (drop (div lenStoneStr 2) stoneStr)]
+  where stoneStr = show stone
+        lenStoneStr = length(stoneStr)
+  
+
+blinkAtList :: [Int] -> [Int]
+blinkAtList stones = stones >>= blinkAtStone
+
 numberOfStonesAfter :: [Int] -> Int -> Int
-numberOfStonesAfter stones numOfBlinks = length(stones)
+numberOfStonesAfter stones numOfBlinks = length(blinkAtList stones)
