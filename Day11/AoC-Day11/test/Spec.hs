c956bac0b6b66dcbb17ad15ca6219fe13c06fe41
Chris Simon <chris@chrissimon.au>
2024-12-11 16:41:48 +1100

    5. RED - handle more than one blink iteration

--- Day11/AoC-Day11/test/Spec.hs
+++ Day11/AoC-Day11/test/Spec.hs
@@ -5,7 +5,8 @@ tests :: Test
 tests = test [ (numberOfStonesAfter [0] 1) @?= 1,
                (numberOfStonesAfter [0, 0] 1) @?= 2,
                (numberOfStonesAfter [10] 1) @?= 2,
-               (numberOfStonesAfter [1] 1) @?= 1
+               (numberOfStonesAfter [1] 1) @?= 1,
+               (numberOfStonesAfter [1000] 2) @?= 3
              ]
 
 main :: IO Counts
