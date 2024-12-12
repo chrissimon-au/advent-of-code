cd72ebbc9f31cb6c901b85f77aa4927eda6b53b3
Chris Simon <chris@devcycles.io>
2024-12-12 17:20:56 +1100

    3. RED - compute a 2-row region

--- Day12/aoc-day12/test/day12_test.clj
+++ Day12/aoc-day12/test/day12_test.clj
@@ -1,13 +1,22 @@
 (ns day12-test 
   (:require
+   [clojure.string :as string]
    [day12]))
 
 (require '[clojure.test :as t])
 
+(defn long-str [& strings] (string/join "\n" strings))
+
+(def t1 (long-str "oo"
+                  "oo"))
+
 (t/deftest test-assertion
   
   (t/testing  "Single square"
-    (t/is (= (day12/fencing-price "o") 4))
-    (t/is (= (day12/fencing-price "oo") 12))))
+    (t/is (= 4 (day12/fencing-price "o"))))
+  (t/testing "Simple row"
+    (t/is (= 12 (day12/fencing-price "oo")))) 
+  (t/testing "grid"
+    (t/is (= 32 (day12/fencing-price t1)))))
 
 (test-assertion)
