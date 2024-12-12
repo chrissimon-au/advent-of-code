ec9d3bec29f2ca960ea95ab27cc4c97babafbdc6
Chris Simon <chris@devcycles.io>
2024-12-13 00:42:14 +1100

    5. GREEN - handle asymmetric structure

--- Day12/aoc-day12/src/day12.clj
+++ Day12/aoc-day12/src/day12.clj
@@ -1,24 +1,27 @@
 (ns day12
   (:require
    [clojure.repl :refer :all]
-   [clojure.string :as string]
-  ))
+   [clojure.string :as string]))
+  
 
 (defn parse [gridMap] (string/split gridMap #"\n"))
 
 (defn area [region] (reduce + (map count region)))
 
-(defn row-circumference [row] (+ 2 (* 2 (count row))))
-
-(defn circumference [region] (- (reduce + (map row-circumference region)) (* 2 (- (count region) 1) (count (get region 0)))))
-
+(defn circumference
+  [region]
+  (+ (* 2 (count region))
+     (* 2 (count (get region 0)))
+  )
+)
 (defn fencing-price-of-grid [grid] (* (area grid) (circumference grid)))
 
 (defn fencing-price [gridMap] (fencing-price-of-grid (parse gridMap)))
 
-(reduce 
- + [6 5])
-()
-(circumference (parse "ooo\nooo"))
-(area (parse "ooo\nooo"))
+
+(circumference (parse "o"))
+(area (parse "ooo\noo"))
 (fencing-price "o")
+(fencing-price "oo")
+(fencing-price "oo\noo")
+(fencing-price "ooo\noo")
