5013578cc6cb7969e33f6d3ad8614952dd4cc6bd
Chris Simon <chris@devcycles.io>
2024-12-12 17:44:28 +1100

    4. GREEN - compute a 2-row region assuming a proper square

--- Day12/aoc-day12/src/day12.clj
+++ Day12/aoc-day12/src/day12.clj
@@ -1,9 +1,24 @@
-(ns day12)
+(ns day12
+  (:require
+   [clojure.repl :refer :all]
+   [clojure.string :as string]
+  ))
 
-(require '[clojure.repl :refer :all])
+(defn parse [gridMap] (string/split gridMap #"\n"))
 
-(defn area [region] (count region))
+(defn area [region] (reduce + (map count region)))
 
-(defn circumference [region] (+ 2 (* 2 (count region))))
+(defn row-circumference [row] (+ 2 (* 2 (count row))))
 
-(defn fencing-price [gridMap] (* (area gridMap) (circumference gridMap)))
+(defn circumference [region] (- (reduce + (map row-circumference region)) (* 2 (- (count region) 1) (count (get region 0)))))
+
+(defn fencing-price-of-grid [grid] (* (area grid) (circumference grid)))
+
+(defn fencing-price [gridMap] (fencing-price-of-grid (parse gridMap)))
+
+(reduce 
+ + [6 5])
+()
+(circumference (parse "ooo\nooo"))
+(area (parse "ooo\nooo"))
+(fencing-price "o")
