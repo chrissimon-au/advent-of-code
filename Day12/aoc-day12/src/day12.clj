65adc9636fe38d116a60d9fec02ab3cc6a9da3a9
Chris Simon <chris@devcycles.io>
2024-12-13 15:42:25 +1100

    8. GREEN - recursively find the independent regions and compute their price

--- Day12/aoc-day12/src/day12.clj
+++ Day12/aoc-day12/src/day12.clj
@@ -1,15 +1,14 @@
 (ns day12
   (:require
-   [clojure.walk :as w]
    [clojure.repl :refer :all]
    [clojure.string :as string]))
   
 
 (defn parse [gridMap] (mapv (fn [x] (string/split x #"")) (string/split gridMap #"\n")))
 
-(defn get-pos [iGrid col row] (filter #(and (= row (% :row)) (= col (% :col))) iGrid))
+(defn get-cell [iGrid col row] (first (filter #(and (= row (% :row)) (= col (% :col))) iGrid)))
 
-(defn get-value [iGrid col row] (let [cell (first (get-pos iGrid col row))]
+(defn get-cell-value [iGrid col row] (let [cell (get-cell iGrid col row)]
                                   (if cell (cell :value) ".")))
 
 (defn indexedGrid [grid] (for [[i row] (map-indexed list grid)
@@ -17,19 +16,17 @@
                                :when (not= "." cell)]
                            {:col i :row j :value (first (char-array cell))}))
 
-(defn cells-in-region [iGrid regionId] (filter #(= (% :value) regionId) iGrid))
-
 (defn area [iRegion] (count iRegion))
 
 (defn nc [value neighbour] (if (not= value neighbour) 1 0))
 
-(defn elem-circumference [iRegion elem] (let [col (elem :col)
-                                              row (elem :row)
-                                              value (elem :value)
-                                              east (get-value iRegion (+ col 1) row)
-                                              west (get-value iRegion (- col 1) row)
-                                              north (get-value iRegion col (- row 1))
-                                              south (get-value iRegion col (+ row 1))]
+(defn elem-circumference [iRegion cell] (let [col (cell :col)
+                                              row (cell :row)
+                                              value (cell :value)
+                                              east (get-cell-value iRegion (+ col 1) row)
+                                              west (get-cell-value iRegion (- col 1) row)
+                                              north (get-cell-value iRegion col (- row 1))
+                                              south (get-cell-value iRegion col (+ row 1))]
                                           (+ (nc value east) (nc value west) (nc value south) (nc value north))))
 
 (defn circumference
@@ -38,19 +35,48 @@
   
 (defn fencing-price-of-grid [iRegion] (* (area iRegion) (circumference iRegion)))
 
-(defn region-ids [gridMap] (filter #(not= % \newline) (distinct gridMap)))
+(defn get-cell-if-in-region [iGrid cell col row] (
+                                                  let [
+                                                       value (cell :value)
+                                                       neighbour (get-cell iGrid col row)
+                                                       neighbourValue (get-cell-value iGrid col row)]
+                                                  (when (= value neighbourValue)
+                                                    neighbour)
+))
 
-(defn fencing-price [gridMap] (
-                               let [
-                                    iGrid (indexedGrid (parse gridMap))
-                                    regionIds (region-ids gridMap)
-                                    regions (map (partial cells-in-region iGrid) regionIds)
-                                    ]
-                               (reduce + (map fencing-price-of-grid regions))
-                              ))
 
-(def ex "o")
-(def iGrid (indexedGrid (parse ex)))
-(def regionIds (region-ids ex))
-(def regions (map (partial cells-in-region iGrid) regionIds))
-(print regions)
+(defn region-from-cell [iGrid cell] (if (= cell nil)
+                                      ['() iGrid]
+                                      (let [col (cell :col)
+                                            row (cell :row)
+                                            newGrid (remove #{cell} iGrid)
+                                            east (get-cell-if-in-region newGrid cell (+ col 1) row)
+                                            [eastTree newGrid] (region-from-cell newGrid east)
+                                            west (get-cell-if-in-region newGrid cell (- col 1) row)
+                                            [westTree newGrid] (region-from-cell newGrid west)
+                                            north (get-cell-if-in-region newGrid cell col (- row 1))
+                                            [northTree newGrid] (region-from-cell newGrid north)
+                                            south (get-cell-if-in-region newGrid cell col (+ row 1))
+                                            [southTree newGrid] (region-from-cell newGrid south)
+                                            ]
+                                       [(cons cell (concat eastTree westTree northTree southTree))
+                                        newGrid])))
+                                      
+                                      
+                                      
+
+(defn split-into-regions [iGrid] (if (empty? iGrid)
+                                   '()
+                                   (let
+                                    [cell (first iGrid)
+                                     [region remainingGrid] (region-from-cell iGrid cell)
+                                     ]
+                                     (conj
+                                      (split-into-regions remainingGrid)
+                                      region
+                                     )))
+  )
+
+(defn fencing-price [gridMap] (let [iGrid (indexedGrid (parse gridMap))
+                                    regions (split-into-regions iGrid)]
+                                (reduce + (map fencing-price-of-grid regions))))
