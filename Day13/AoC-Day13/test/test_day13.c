aa258cae7a426ad4688a2bb1def13a9076b1e467
Chris Simon <chris@chrissimon.au>
2024-12-13 21:47:43 +1100

    7. RED - add multiple instructions in the string

--- Day13/AoC-Day13/test/test_day13.c
+++ Day13/AoC-Day13/test/test_day13.c
@@ -24,6 +24,12 @@ void test_simple2()
     TEST_ASSERT_EQUAL(200, output);
 }
 
+void test_multiple()
+{
+    int output = cost_to_win_from_instructions("Button A: X+94, Y+34\nButton B: X+22, Y+67\nPrize: X=8400, Y=5400\n\nButton A: X+26, Y+66\nButton B: X+67, Y+21\nPrize: X=12748, Y=12176\n\nButton A: X+17, Y+86\nButton B: X+84, Y+37\nPrize: X=7870, Y=6450\n\nButton A: X+69, Y+23\nButton B: X+27, Y+71\nPrize: X=18641, Y=10279");
+    TEST_ASSERT_EQUAL(480, output);
+}
+
 void test_simple_game()
 {
     Game game;
