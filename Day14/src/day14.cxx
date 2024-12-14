e99b36b9d44f54b48776d867195de525c3512e3d
Chris Simon <chris@chrissimon.au>
2024-12-14 16:50:25 +1100

    3. RED - simple case for directly controlling a robot

--- Day14/src/day14.cxx
+++ Day14/src/day14.cxx
@@ -12,24 +12,55 @@ public:
     const int y() const { return y_; }
 };
 
-struct Position : Coordinates {
-    Position(int x, int y) : Coordinates(x,y) {}
-};
-
 std::ostream & operator << (std::ostream & outs, const Coordinates & coords) {
     return outs << "(" << coords.x() << ", " << coords.y() << ")";
 }
+
+class Position : public Coordinates {
+public:
+    Position(int x, int y) : Coordinates(x,y) {}
+};
+
+class Velocity : public Coordinates {
+public:
+    Velocity(int x, int y) : Coordinates(x,y) {}
+};
+
+class Robot {
+private:
+    Position position_;
+    Velocity velocity_;
+    Position map_size_;
+public:
+    Robot(Position position, Velocity velocity, Position map_size) : position_(position), velocity_(velocity), map_size_(map_size) {}
+    const Position position() const { return position_; }
+
+    void move_seconds(int seconds) { 
+
+    }
+};
 
 int get_safety_score(std::string input, Position size) {
     return 0;
 }
 
-
 TEST_CASE( "Single Robot, Single Dimension" ) {
     SECTION( "From Input" ) {
         std::string input = "p=0,4 v=1,0";
-        Position size = Position(7,5);
-        std::cout << size << std::endl;
-        REQUIRE( get_safety_score(input, size) == 0 );
+        Position map_size = Position(7,5);
+        std::cout << map_size << std::endl;
+        REQUIRE( get_safety_score(input, map_size) == 0 );
+    }
+
+    SECTION( "Direct Robot" ) {
+        Position map_size = Position(7,5);
+        Position position = Position(0,4);
+        Velocity velocity = Velocity(1,0);
+        Robot r = Robot(position, velocity, map_size);
+
+        r.move_seconds(1);
+
+        REQUIRE( r.position().x() == 1 );
+        REQUIRE( r.position().y() == 4 );
     }
 }
