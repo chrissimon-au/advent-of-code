872eb602086b4d02c53c20197258d85915ea8c61
Chris Simon <chris@devcycles.io>
2024-12-14 23:38:16 +1100

    12. RED - add AoC sample data

--- Day14/src/day14.cxx
+++ Day14/src/day14.cxx
@@ -50,7 +50,9 @@ public:
     const Position position() const { return position_; }
 
     void move_seconds(int seconds, Position map_size) { 
+        std::cout << "Moving from " << position_ << " with " << seconds << ", " << velocity_ << " wrapping at " << map_size << ":" << std::endl;
         position_.move_by(velocity_ * seconds, map_size);
+        std::cout << "  " << position_ << std::endl;
     }
 };
 
@@ -60,6 +62,12 @@ private:
     std::vector<Robot> robots_;
 public:
     Map(Position size) : size_(size) {}
+    void move_seconds(int seconds) {
+    for (auto r : robots_)
+        {
+            r.move_seconds(seconds, size_);
+        }
+    }
     int safety_score() {
         int top_left = 0;
         int top_right = 0;
@@ -83,6 +91,7 @@ public:
                 bottom_right++;
             }
         }
+        std::cout << "top_left: " << top_left << "; top_right: " << top_right
         return top_left * top_right * bottom_left * bottom_right;
     }
     void add_robot(const Robot robot) {
@@ -95,114 +104,134 @@ int get_safety_score(std::string input, Position size) {
 }
 
 TEST_CASE( "Single Robot, Single Dimension" ) {
-    SECTION( "From Input" ) {
-        std::string input = "p=0,4 v=1,0";
-        Position map_size = Position(7,5);
-        std::cout << map_size << std::endl;
-        REQUIRE( get_safety_score(input, map_size) == 0 );
-    }
+//     SECTION( "From Input" ) {
+//         std::string input = "p=0,4 v=1,0";
+//         Position map_size = Position(7,5);
+//         std::cout << map_size << std::endl;
+//         REQUIRE( get_safety_score(input, map_size) == 0 );
+//     }
 
     SECTION( "Direct Robot" ) {
 
-        SECTION( "moving a single time unit" ) {
-            Position map_size = Position(7,5);
-            Position position = Position(0,4);
-            Velocity velocity = Velocity(1,0);
-            Robot r = Robot(position, velocity);
+//         SECTION( "moving a single time unit" ) {
+//             Position map_size = Position(7,5);
+//             Position position = Position(0,4);
+//             Velocity velocity = Velocity(1,0);
+//             Robot r = Robot(position, velocity);
 
-            r.move_seconds(1, map_size);
+//             r.move_seconds(1, map_size);
 
-            REQUIRE( r.position().x() == 1 );
-            REQUIRE( r.position().y() == 4 );
-        }
+//             REQUIRE( r.position().x() == 1 );
+//             REQUIRE( r.position().y() == 4 );
+//         }
 
-        SECTION( "moving multiple time units" ) {
-            Position map_size = Position(7,5);
-            Position position = Position(0,4);
-            Velocity velocity = Velocity(1,0);
-            Robot r = Robot(position, velocity);
+//         SECTION( "moving multiple time units" ) {
+//             Position map_size = Position(7,5);
+//             Position position = Position(0,4);
+//             Velocity velocity = Velocity(1,0);
+//             Robot r = Robot(position, velocity);
 
-            r.move_seconds(4, map_size);
+//             r.move_seconds(4, map_size);
 
-            REQUIRE( r.position().x() == 4 );
-            REQUIRE( r.position().y() == 4 );
-        }
+//             REQUIRE( r.position().x() == 4 );
+//             REQUIRE( r.position().y() == 4 );
+//         }
 
-        SECTION( "moving multiple time units and wrapping" ) {
-            Position map_size = Position(7,5);
-            Position position = Position(0,4);
-            Velocity velocity = Velocity(1,0);
-            Robot r = Robot(position, velocity);
+//         SECTION( "moving multiple time units and wrapping" ) {
+//             Position map_size = Position(7,5);
+//             Position position = Position(0,4);
+//             Velocity velocity = Velocity(1,0);
+//             Robot r = Robot(position, velocity);
 
-            r.move_seconds(8, map_size);
+//             r.move_seconds(8, map_size);
 
-            REQUIRE( r.position().x() == 1 );
-            REQUIRE( r.position().y() == 4 );
-        }
+//             REQUIRE( r.position().x() == 1 );
+//             REQUIRE( r.position().y() == 4 );
+//         }
 
-        SECTION( "moving in both directions and wrapping" ) {
-            Position map_size = Position(7,5);
-            Position position = Position(0,4);
-            Velocity velocity = Velocity(1,3);
-            Robot r = Robot(position, velocity);
+//         SECTION( "moving in both directions and wrapping" ) {
+//             Position map_size = Position(7,5);
+//             Position position = Position(0,4);
+//             Velocity velocity = Velocity(1,3);
+//             Robot r = Robot(position, velocity);
 
-            r.move_seconds(8, map_size);
+//             r.move_seconds(8, map_size);
 
-            REQUIRE( r.position().x() == 1 );
-            REQUIRE( r.position().y() == 3 );
-        }
+//             REQUIRE( r.position().x() == 1 );
+//             REQUIRE( r.position().y() == 3 );
+//         }
 
-        SECTION( "moving backwards" ) {
-            Position map_size = Position(7,5);
-            Position position = Position(0,4);
-            Velocity velocity = Velocity(-1,-3);
-            Robot r = Robot(position, velocity);
+        // SECTION( "moving backwards" ) {
+        //     Position map_size = Position(7,5);
+        //     Position position = Position(0,4);
+        //     Velocity velocity = Velocity(-1,-3);
+        //     Robot r = Robot(position, velocity);
 
-            r.move_seconds(2, map_size);
+        //     r.move_seconds(2, map_size);
 
-            REQUIRE( r.position().x() == 5 );
-            REQUIRE( r.position().y() == 3 );
-        }
+        //     REQUIRE( r.position().x() == 5 );
+        //     REQUIRE( r.position().y() == 3 );
+        // }
 
     }
 }
 
 TEST_CASE( "Map can compute safety score" ) {
-    SECTION( "1 Robot in each quadrant" ) {
-        Map map = Map(Position(7,5));
-        map.add_robot(Robot(Position(0,0), Velocity(0,0)));
-        map.add_robot(Robot(Position(6,0), Velocity(0,0)));
-        map.add_robot(Robot(Position(0,4), Velocity(0,0)));
-        map.add_robot(Robot(Position(6,4), Velocity(0,0)));
+    // SECTION( "1 Robot in each quadrant" ) {
+    //     Map map = Map(Position(7,5));
+    //     map.add_robot(Robot(Position(0,0), Velocity(0,0)));
+    //     map.add_robot(Robot(Position(6,0), Velocity(0,0)));
+    //     map.add_robot(Robot(Position(0,4), Velocity(0,0)));
+    //     map.add_robot(Robot(Position(6,4), Velocity(0,0)));
 
-        REQUIRE( map.safety_score() == 1 );
-    }
+    //     REQUIRE( map.safety_score() == 1 );
+    // }
 
-    SECTION( "2 Robot in each quadrant" ) {
-        Map map = Map(Position(7,5));
-        map.add_robot(Robot(Position(0,0), Velocity(0,0)));
-        map.add_robot(Robot(Position(1,1), Velocity(0,0)));
-        map.add_robot(Robot(Position(6,0), Velocity(0,0)));
-        map.add_robot(Robot(Position(5,1), Velocity(0,0)));
-        map.add_robot(Robot(Position(0,4), Velocity(0,0)));
-        map.add_robot(Robot(Position(1,3), Velocity(0,0)));
-        map.add_robot(Robot(Position(6,4), Velocity(0,0)));
-        map.add_robot(Robot(Position(5,3), Velocity(0,0)));
+    // SECTION( "2 Robot in each quadrant" ) {
+    //     Map map = Map(Position(7,5));
+    //     map.add_robot(Robot(Position(0,0), Velocity(0,0)));
+    //     map.add_robot(Robot(Position(1,1), Velocity(0,0)));
+    //     map.add_robot(Robot(Position(6,0), Velocity(0,0)));
+    //     map.add_robot(Robot(Position(5,1), Velocity(0,0)));
+    //     map.add_robot(Robot(Position(0,4), Velocity(0,0)));
+    //     map.add_robot(Robot(Position(1,3), Velocity(0,0)));
+    //     map.add_robot(Robot(Position(6,4), Velocity(0,0)));
+    //     map.add_robot(Robot(Position(5,3), Velocity(0,0)));
 
-        REQUIRE( map.safety_score() == 16 );
-    }
+    //     REQUIRE( map.safety_score() == 16 );
+    // }
 
-    SECTION( "Robot on centre line don't count" ) {
-        Map map = Map(Position(7,5));
-        map.add_robot(Robot(Position(0,0), Velocity(0,0)));
-        map.add_robot(Robot(Position(3,1), Velocity(0,0)));
-        map.add_robot(Robot(Position(6,0), Velocity(0,0)));
-        map.add_robot(Robot(Position(3,1), Velocity(0,0)));
-        map.add_robot(Robot(Position(0,4), Velocity(0,0)));
-        map.add_robot(Robot(Position(1,2), Velocity(0,0)));
-        map.add_robot(Robot(Position(6,4), Velocity(0,0)));
-        map.add_robot(Robot(Position(5,2), Velocity(0,0)));
+    // SECTION( "Robot on centre line don't count" ) {
+    //     Map map = Map(Position(7,5));
+    //     map.add_robot(Robot(Position(0,0), Velocity(0,0)));
+    //     map.add_robot(Robot(Position(3,1), Velocity(0,0)));
+    //     map.add_robot(Robot(Position(6,0), Velocity(0,0)));
+    //     map.add_robot(Robot(Position(3,1), Velocity(0,0)));
+    //     map.add_robot(Robot(Position(0,4), Velocity(0,0)));
+    //     map.add_robot(Robot(Position(1,2), Velocity(0,0)));
+    //     map.add_robot(Robot(Position(6,4), Velocity(0,0)));
+    //     map.add_robot(Robot(Position(5,2), Velocity(0,0)));
 
-        REQUIRE( map.safety_score() == 1 );
+    //     REQUIRE( map.safety_score() == 1 );
+    // }
+
+    SECTION( "AoC Sample Case" ) {
+        Map map = Map(Position(11,7));
+        map.add_robot(Robot(Position(0,4), Velocity(3,-3)));
+        map.add_robot(Robot(Position(6,3), Velocity(-1,-3)));
+        map.add_robot(Robot(Position(10,3), Velocity(-1,2)));
+        map.add_robot(Robot(Position(2,0), Velocity(2,-1)));
+        map.add_robot(Robot(Position(0,0), Velocity(1,3)));
+        map.add_robot(Robot(Position(3,0), Velocity(-2,-2)));
+        map.add_robot(Robot(Position(7,6), Velocity(-1,-3)));
+        map.add_robot(Robot(Position(3,0), Velocity(-1,-2)));
+        map.add_robot(Robot(Position(9,3), Velocity(2,3)));
+        map.add_robot(Robot(Position(7,3), Velocity(-1,2)));
+        map.add_robot(Robot(Position(2,4), Velocity(2,-3)));
+        map.add_robot(Robot(Position(9,5), Velocity(-3,-3)));
+
+        map.move_seconds(100);
+
+        REQUIRE(map.safety_score() == 12);
     }
 }
