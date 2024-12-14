dc118cc08173f529605dff43024121d99da6aa58
Chris Simon <chris@chrissimon.au>
2024-12-14 16:19:30 +1100

    2. GREEN - Add configurable map size

--- Day14/src/day14.cxx
+++ Day14/src/day14.cxx
@@ -1,11 +1,24 @@
 #include <catch2/catch_test_macros.hpp>
+#include <bits/stdc++.h>
 #include <string>
 
-int get_safety_score(std::string input) {
+struct Coordinates {
+    int x;
+    int y;
+    Coordinates(int x, int y) : x(x), y(y) {}
+};
+std::ostream & operator << (std::ostream & outs, const Coordinates & coords) {
+    return outs << "(" << coords.x << ", " << coords.y << ")";
+}
+
+int get_safety_score(std::string input, Coordinates size) {
     return 0;
 }
 
+
 TEST_CASE( "Single Robot, Single Dimension" ) {
-    std::string input = "p=0,4 v=1,0";    
-    REQUIRE( get_safety_score(input) == 0 );
+    std::string input = "p=0,4 v=1,0";
+    Coordinates size = Coordinates(7,5);
+    std::cout << size << std::endl;
+    REQUIRE( get_safety_score(input, size) == 0 );
 }
