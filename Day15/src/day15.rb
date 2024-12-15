cc28fa838b00dbdc953bc458414fe8eb346859b3
Chris Simon <chris@chrissimon.au>
2024-12-15 18:38:15 +1100

    1. RED - Initial test of grid layout

--- Day15/src/day15.rb
+++ Day15/src/day15.rb
@@ -1,8 +1,22 @@
 require 'test/unit'
 
-def check
-    true
+class Coordinates
+  def initialize(x, y)    
+  end
+  def x
+  end
 end
+
+class Grid
+  @size
+  def initialize(size)
+  end
+
+  def size
+    Coordinates.new(0, 0)
+  end
+end
+   
 
 
 class MyTest < Test::Unit::TestCase
@@ -12,7 +26,10 @@ class MyTest < Test::Unit::TestCase
   # def teardown
   # end
 
-  def test_check
-    assert(check())
+  def test_grid_layout
+    grid = Grid.new(Coordinates.new(6, 7))
+
+    assert_equal(6, grid.size.x)
+    
   end
 end
