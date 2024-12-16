5469eb57191383ebab9a664e3f631232650305ab
Chris Simon <chris@chrissimon.au>
2024-12-16 11:33:08 +1100

    22. RED - add aoc sample, test and extra edge cases

--- Day15/src/day15_part2_tests.rb
+++ Day15/src/day15_part2_tests.rb
@@ -76,4 +76,83 @@ class MyTest < Test::Unit::TestCase
         assert_equal(9, grid.robot.x)
         assert_equal(2, grid.robot.y)
     end
+
+    def test_single_gap
+        map_input = <<~EOS
+                ########
+                #O.O@..#
+                #......#
+                ########
+
+                <<
+                EOS
+        grid = Grid.parse(map_input, 2)
+
+        assert(grid.is_box_at(Coordinates.new(2,1)))
+        assert(grid.is_box_at(Coordinates.new(3,1)))
+        assert(grid.is_box_at(Coordinates.new(4,1)))
+        assert(grid.is_box_at(Coordinates.new(5,1)))
+        assert_equal(6, grid.robot.x)
+        assert_equal(1, grid.robot.y)
+    end
+
+    def test_wcabus
+        map_input = <<~EOS
+                    #######
+                    #...#.#
+                    #.....#
+                    #.....#
+                    #.....#
+                    #.....#
+                    #.OOO@#
+                    #.OOO.#
+                    #..O..#
+                    #.....#
+                    #.....#
+                    #######
+
+                    v<vv<<^^^^^
+                    EOS
+        grid = Grid.parse(map_input, 2)
+
+        assert_equal(2339, grid.gps)
+    end
+
+    def test_corner
+        map_input = <<~EOS
+                    #######
+                    #.....#
+                    #.....#
+                    #.@O..#
+                    #..#O.#
+                    #...O.#
+                    #..O..#
+                    #.....#
+                    #######
+
+                    >><vvv>v>^^^
+                    EOS
+
+        grid = Grid.parse(map_input, 2, false)
+
+        assert_equal(1430, grid.gps)
+    end
+
+    def test_aoc_sample
+        map_input = File.read("../sampledata.txt")
+        expected = Integer(File.read("../sampledata.answer2.txt"))
+
+        grid = Grid.parse(map_input, 2)
+
+        assert_equal(expected, grid.gps)
+    end
+
+    def test_aoc_test
+        map_input = File.read("../testdata.txt")
+        expected = Integer(File.read("../testdata.answer2.txt"))
+
+        grid = Grid.parse(map_input, 2)
+
+        assert_equal(expected, grid.gps)
+    end
 end
