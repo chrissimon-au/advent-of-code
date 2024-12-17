578f79f350a75b6c7a093b91b6497e1466d9e2c4
Chris Simon <chris@chrissimon.au>
2024-12-18 09:02:53 +1100

    4. GREEN - bxl no-op case passing

--- Day17/day17.go
+++ Day17/day17.go
@@ -6,15 +6,48 @@ import (
 	"strings"
 )
 
+type Registers struct {
+	A int
+	B int
+	C int
+}
+
+func adv(registers Registers, operand int) (Registers, int) {
+	divisor := int(math.Pow(2, float64(operand)))
+	return registers, registers.A / divisor
+}
+
+func bxl(registers Registers, _ int) (Registers, int) {
+	return registers, 0
+}
+
+func EvaluateOp(registers Registers, opcode int, operand int) (Registers, int) {
+	switch opcode {
+	case 0:
+		return adv(registers, operand)
+	case 1:
+		return bxl(registers, operand)
+	}
+	return registers, 0
+}
+
 func ExecuteProgram(input string) string {
 	inputParts := strings.Split(input, "\n\n")
-	registers := strings.Split(inputParts[0], "\n")
-	instructions := strings.Split(inputParts[1], ",")
-	registerAStr := strings.Replace(registers[0], "Register A: ", "", -1)
+	registerStrs := strings.Split(inputParts[0], "\n")
+	instructions := strings.Split(strings.Replace(inputParts[1], "Program: ", "", -1), ",")
+	registerAStr := strings.Replace(registerStrs[0], "Register A: ", "", -1)
 	registerA, _ := strconv.Atoi(registerAStr)
-	divisorPower, _ := strconv.Atoi(instructions[1])
-	divisor := int(math.Pow(2, float64(divisorPower)))
-	result := registerA / divisor
+	opcode, _ := strconv.Atoi(instructions[0])
+	operand, _ := strconv.Atoi(instructions[1])
+
+	registers := Registers{registerA, 0, 0}
+	// fmt.Println("====")
+	// fmt.Printf("%s\n", input)
+	// fmt.Printf("%s: %s\n", registerAStr, instructions)
+	// fmt.Printf("%d: %d %d\n", registers, opcode, operand)
+	// fmt.Println("---")
+
+	_, result := EvaluateOp(registers, opcode, operand)
 
 	return strconv.Itoa(result)
 }
