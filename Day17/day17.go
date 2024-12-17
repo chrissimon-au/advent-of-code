3a1eee99f01ee79f5e5dd600cb606e9873277780
Chris Simon <chris@chrissimon.au>
2024-12-18 08:39:54 +1100

    2. GREEN - pass case that does division

--- Day17/day17.go
+++ Day17/day17.go
@@ -1,9 +1,22 @@
 package day17
 
 import (
-
+	"fmt"
+	"math"
+	"strconv"
+	"strings"
 )
 
 func ExecuteProgram(input string) string {
-	return "0"
+	inputParts := strings.Split(input, "\n\n")
+	registers := strings.Split(inputParts[0], "\n")
+	instructions := strings.Split(inputParts[1], ",")
+	registerAStr := strings.Replace(registers[0], "Register A: ", "", -1)
+	registerA, _ := strconv.Atoi(registerAStr)
+	divisorPower, _ := strconv.Atoi(instructions[1])
+	divisor := int(math.Pow(2, float64(divisorPower)))
+	result := registerA / divisor
+	fmt.Printf("%s: %d %d %d %d\n", registerAStr, registerA, divisorPower, divisor, result)
+
+	return strconv.Itoa(result)
 }
