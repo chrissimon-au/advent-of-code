60064596df51b3c3899a21399ff61ce3c411f78c
Chris Simon <chris@chrissimon.au>
2024-12-18 08:27:47 +1100

    2. RED - add case that actually does division

--- Day17/day17_test.go
+++ Day17/day17_test.go
@@ -1,9 +1,9 @@
 package day17
 
 import (
-	"testing"
 	"fmt"
 	"strings"
+	"testing"
 )
 
 func expect_equal(t *testing.T, expected interface{}, actual interface{}) {
@@ -14,25 +14,32 @@ func expect_equal(t *testing.T, expected interface{}, actual interface{}) {
 
 func Test_adv(t *testing.T) {
 	var tests = []struct {
-        input string
-        expected_output string
-    }{
-        {`
+		input           string
+		expected_output string
+	}{
+		{`
 Register A: 0
 Register B: 0
 Register C: 0
 
 Program: 0,1,5,4`, "0"},
-    }
+
+		{`
+Register A: 4
+Register B: 0
+Register C: 0
+
+Program: 0,1,5,4`, "2"},
+	}
 
 	for _, tt := range tests {
-		var input = strings.TrimSpace(tt.input)
-        testname := fmt.Sprintf("Test_adv: '%s','%s'", input, tt.expected_output)
-        t.Run(testname, func(t *testing.T) {
-			
+		input := strings.TrimSpace(tt.input)
+		testname := fmt.Sprintf("Test_adv: '%s','%s'", input, tt.expected_output)
+		t.Run(testname, func(t *testing.T) {
+
 			var result = ExecuteProgram(input)
 
 			expect_equal(t, tt.expected_output, result)
-        })
-    }
+		})
+	}
 }
