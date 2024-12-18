a74c07260a610be763cf9b28c0ce6bb4bbfd3395
Chris Simon <chris@chrissimon.au>
2024-12-18 21:20:03 +1100

    2. RED - test with aoc case which will require a graph algo

--- Day18/day18/src/lib.rs
+++ Day18/day18/src/lib.rs
@@ -5,10 +5,18 @@ pub fn shortest_path(width: i32, height: i32, _input: &str) -> i32 {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::fs;
+    use std::str::FromStr;
 
     #[test]
-    fn it_works() {
+    fn empty_gred() {
         let result = shortest_path(6, 6, "");
         assert_eq!(result, 12);
+    }
+
+    #[test]
+    fn sample() {
+        let result = shortest_path(6, 6, &fs::read_to_string("../sampledata.txt").unwrap());
+        assert_eq!(result, i32::from_str(&fs::read_to_string("../sampledata.answer.txt").unwrap()).unwrap());
     }
 }
