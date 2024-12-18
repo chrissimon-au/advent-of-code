8fc02d413553550e41561b160d2f541890a03de0
Chris Simon <chris@chrissimon.au>
2024-12-18 22:22:16 +1100

    3. GREEN - passing when only consuming a limited number of bytes

--- Day18/day18/src/lib.rs
+++ Day18/day18/src/lib.rs
@@ -1,9 +1,9 @@
 use pathfinding::prelude::bfs;
 use std::str::FromStr;
 
-fn parse_input(input: &str) -> Vec<(i32,i32)> {
+fn parse_input(input: &str, byte_limit: usize) -> Vec<(i32,i32)> {
     let mut corrupted : Vec<(i32,i32)> = Vec::new();
-    for location in input.lines() {
+    for location in input.lines().take(byte_limit) {
         let mut coords = location.split(",");
         let x = i32::from_str(coords.next().unwrap()).unwrap();
         let y = i32::from_str(coords.next().unwrap()).unwrap();
@@ -12,14 +12,17 @@ fn parse_input(input: &str) -> Vec<(i32,i32)> {
     return corrupted;
 }
 
-pub fn shortest_path(width: i32, height: i32, input: &str) -> usize {
-    let corrupted = parse_input(input);
+pub fn shortest_path(width: i32, height: i32, input: &str, byte_limit: usize) -> usize {
+    let corrupted = parse_input(input, byte_limit);
 
     let result = bfs(&(0, 0),
         |&(x, y)| {
             let mut v : Vec<(i32,i32)> = vec![];
-
-            let mut add_if_not_corrupted = |p| {if !corrupted.contains(p) { v.push(*p) } };
+            let mut add_if_not_corrupted = |&(xa,ya)| {
+                if xa >= 0 && xa <= width && ya >= 0 && ya <= height && !corrupted.contains(&(xa,ya)) {
+                    v.push((xa,ya))
+                } 
+            };
             let n = (x+1,y);
             add_if_not_corrupted(&n);
             let n = (x-1,y);
@@ -31,9 +34,11 @@ pub fn shortest_path(width: i32, height: i32, input: &str) -> usize {
 
             return v;
         },
-        |&p| p == (width-1,height-1));
-    return result.expect("no path found").len()+1;
+        |&p| p == (width,height));
+
+    return result.expect("no path found").len()-1;
 }
+
 
 #[cfg(test)]
 mod tests {
@@ -42,14 +47,20 @@ mod tests {
     use std::str::FromStr;
 
     #[test]
-    fn empty_gred() {
-        let result = shortest_path(6, 6, "");
+    fn empty_grid() {
+        let result = shortest_path(6, 6, "", 0);
         assert_eq!(result, 12);
     }
 
     #[test]
-    fn sample() {
-        let result = shortest_path(6, 6, &fs::read_to_string("../sampledata.txt").unwrap());
+    fn aoc_sample() {
+        let result = shortest_path(6, 6, &fs::read_to_string("../sampledata.txt").unwrap(), 12);
         assert_eq!(result, usize::from_str(&fs::read_to_string("../sampledata.answer.txt").unwrap()).unwrap());
+    }
+
+    #[test]
+    fn aoc_test() {
+        let result = shortest_path(70, 70, &fs::read_to_string("../testdata.txt").unwrap(), 1024);
+        assert_eq!(result, usize::from_str(&fs::read_to_string("../testdata.answer.txt").unwrap()).unwrap());
     }
 }
