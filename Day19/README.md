7a91220cdafb2a23a708b9fb7079927d9bfc3975
Chris Simon <chris@chrissimon.au>
2024-12-18 23:16:34 +1100

    Prepping for Day 19 in picat

---
+++ Day19/README.md
@@ -0,0 +1,7 @@
+## Day 19: ???
+
+https://adventofcode.com/2024/day/19
+
+TBD in [Picat](https://picat-lang.org/).
+
+To start, run `devenv up` in this folder.
---
+++ Day19/devenv.nix
@@ -0,0 +1,26 @@
+{ pkgs, lib, config, inputs, ... }:
+
+{
+  # https://devenv.sh/packages/
+  packages = [
+    pkgs.git
+    pkgs.watchexec
+    pkgs.picat
+  ];
+
+  # https://devenv.sh/languages/
+  # languages.???.enable = true;  
+
+  # https://devenv.sh/processes/
+  processes.code.exec = "code .";
+  # process.managers.process-compose.settings = {
+  #   processes = {
+  #     test-wait = {
+  #       command = "watchexec -e txt './command.sh'";
+  #       is_tty = true;
+  #     };
+  #   };
+  # };
+
+  cachix.enable = false;
+}
