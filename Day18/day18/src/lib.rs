ec4152c0c3435c53d72f7da40439810f6f1a3f65
Chris Simon <chris@chrissimon.au>
2024-12-18 22:01:42 +1100

    2. GREEN - test case passing with pathfinding crate

--- Day18/day18/src/lib.rs
+++ Day18/day18/src/lib.rs
@@ -1,5 +1,38 @@
-pub fn shortest_path(width: i32, height: i32, _input: &str) -> i32 {
-    return width + height;
+use pathfinding::prelude::bfs;
+use std::str::FromStr;
+
+fn parse_input(input: &str) -> Vec<(i32,i32)> {
+    let mut corrupted : Vec<(i32,i32)> = Vec::new();
+    for location in input.lines() {
+        let mut coords = location.split(",");
+        let x = i32::from_str(coords.next().unwrap()).unwrap();
+        let y = i32::from_str(coords.next().unwrap()).unwrap();
+        corrupted.push((x,y));
+    }
+    return corrupted;
+}
+
+pub fn shortest_path(width: i32, height: i32, input: &str) -> usize {
+    let corrupted = parse_input(input);
+
+    let result = bfs(&(0, 0),
+        |&(x, y)| {
+            let mut v : Vec<(i32,i32)> = vec![];
+
+            let mut add_if_not_corrupted = |p| {if !corrupted.contains(p) { v.push(*p) } };
+            let n = (x+1,y);
+            add_if_not_corrupted(&n);
+            let n = (x-1,y);
+            add_if_not_corrupted(&n);
+            let n = (x,y-1);
+            add_if_not_corrupted(&n);
+            let n = (x,y+1);
+            add_if_not_corrupted(&n);
+
+            return v;
+        },
+        |&p| p == (width-1,height-1));
+    return result.expect("no path found").len()+1;
 }
 
 #[cfg(test)]
@@ -17,6 +50,6 @@ mod tests {
     #[test]
     fn sample() {
         let result = shortest_path(6, 6, &fs::read_to_string("../sampledata.txt").unwrap());
-        assert_eq!(result, i32::from_str(&fs::read_to_string("../sampledata.answer.txt").unwrap()).unwrap());
+        assert_eq!(result, usize::from_str(&fs::read_to_string("../sampledata.answer.txt").unwrap()).unwrap());
     }
 }
