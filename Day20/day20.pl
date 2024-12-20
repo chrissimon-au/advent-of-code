84f39fd1a2921e17c06141aab3b7b911c40f1b3b
Chris Simon <chris@chrissimon.au>
2024-12-21 08:47:07 +1100

    1. RED - simple case with just one available cheat

--- Day20/day20.pl
+++ Day20/day20.pl
@@ -2,12 +2,35 @@ package My::Day20;
 
 use strict;
 use warnings;
-use builtin qw(true false);
-use Test::Simple tests => 1;
-
-my $foo = true;
-
+use builtin qw(true false trim);
+use feature qw(say);
+use Test::Simple tests => 3;
 
 
+sub count_cheats_saving_at_least {
+    my $maze = $_[0];
+    my $limit = $_[1];
+    say $maze;
+    return 0;
+}
 
-ok ($foo eq true);
+
+ok (count_cheats_saving_at_least(trim('
+####
+#SE#
+####
+'),0) eq 0);
+
+ok (count_cheats_saving_at_least(trim('
+#####
+#S#E#
+#...#
+#####
+'),5) eq 0);
+
+ok (count_cheats_saving_at_least(trim('
+#####
+#S#E#
+#...#
+#####
+'),2) eq 1);
