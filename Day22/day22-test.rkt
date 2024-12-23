dcf95ef22c4f724fd1a1f5921d65d22fba62371e
Chris Simon <chris@chrissimon.au>
2024-12-23 17:57:29 +1100

    5. RED Add test of n iterations of secret numbers

--- Day22/day22-test.rkt
+++ Day22/day22-test.rkt
@@ -17,3 +17,8 @@
 (check-eq? (next-secret 11100544) 12249484)
 (check-eq? (next-secret 12249484) 7753432)
 (check-eq? (next-secret 7753432) 5908254)
+
+(check-eq? (nth-secret 1 2000) 8685429)
+; (check-eq? (nth-secret 10 2000) 4700978)
+; (check-eq? (nth-secret 100 2000) 15273692)
+; (check-eq? (nth-secret 2024 2000) 8667524)
