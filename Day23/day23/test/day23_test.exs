dcac30cd24999cabd0262ea71b5a3077864546e8
Chris Simon <chris@chrissimon.au>
2024-12-24 06:08:20 +1100

    5. REFACTOR - prep sample test for also testing test

--- Day23/day23/test/day23_test.exs
+++ Day23/day23/test/day23_test.exs
@@ -20,43 +20,14 @@ bp-kh
     assert Enum.sort(Graph.vertices(g)) == Enum.sort(["kh", "tc", "ba", "bp"])
   end
 
-  test "finds number of game-sets with t" do
-    g = Day23.make_graph("""
-kh-tc
-qp-kh
-de-cg
-ka-co
-yn-aq
-qp-ub
-cg-tb
-vc-aq
-tb-ka
-wh-tc
-yn-cg
-kh-ub
-ta-co
-de-co
-tc-td
-tb-wq
-wh-td
-ta-ka
-td-qp
-aq-cg
-wq-ub
-ub-vc
-de-ta
-wq-aq
-wq-vc
-wh-yn
-ka-de
-kh-ta
-co-tc
-wh-qp
-tb-vc
-td-yn
-""")
+  def assert_game_sets(input, expected_gameset) do
+    g = Day23.make_graph(input)
     assert Graph.num_vertices(g) == 16
     assert Graph.num_edges(g) == 32
-    assert Day23.num_gamesets(g, "t") == 7
+    assert Day23.num_gamesets(g, "t") == expected_gameset |> String.to_integer
+  end
+
+  test "AoC Sample: finds number of game-sets with t" do
+    assert_game_sets(File.read!("../sampledata.txt"), File.read!("../sampledata.answer.txt"))
   end
 end
