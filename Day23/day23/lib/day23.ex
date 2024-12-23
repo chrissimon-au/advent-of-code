df974f9c5db530df903e30891a2190fe33abfc0d
Chris Simon <chris@chrissimon.au>
2024-12-24 00:47:09 +1100

    4. GREEN - can construct more complex graph

--- Day23/day23/lib/day23.ex
+++ Day23/day23/lib/day23.ex
@@ -3,23 +3,16 @@ defmodule Day23 do
   Documentation for `Day23`.
   """
 
-  def add_connection(g, connection) do
-    IO.puts("h: #{connection}")
-    vertices = String.split(connection, "-")
-    IO.inspect(vertices)
-    edge = List.to_tuple(vertices)
-    IO.inspect(edge)
-    Graph.add_vertices(g, vertices)
-    Graph.add_edges(g, [edge])
-    IO.inspect(Graph.vertices(g))
-    IO.inspect(Graph.num_edges(g))
+  def get_edge(connection) do
+    String.split(connection,"-")
+    |> List.to_tuple
   end
 
+  @spec make_graph(String.t()) :: Graph.t()
   def make_graph(input) do
     g = Graph.new()
     connections = String.split(String.trim(input), "\n")
-    add_connection(g, Enum.at(connections, 0))
-    # Enum.each(connections, fn c -> add_connection(g, c) end)
-    g
+    edges = Enum.map(connections, &get_edge/1)
+    Graph.add_edges(g, edges)
   end
 end
