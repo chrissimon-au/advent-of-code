fbd0125a52418894fc9ffc4c8246fdc97b2fa97e
Chris Simon <chris@chrissimon.au>
2024-12-24 00:16:44 +1100

    4. RED - can build graph of multiple nodes/edges

--- Day23/day23/test/day23_test.exs
+++ Day23/day23/test/day23_test.exs
@@ -2,8 +2,19 @@ defmodule Day23Test do
   use ExUnit.Case
   doctest Day23
 
-  test "constructs a graph" do
+  test "constructs a graph of a single connect" do
     g = Day23.make_graph("kh-tc");
+    assert Graph.num_vertices(g) == 2
+    assert Graph.num_edges(g) == 1
+    assert Enum.sort(Graph.vertices(g)) == Enum.sort(["kh", "tc"])
+  end
+
+  test "constructs a graph of multiple connects" do
+    g = Day23.make_graph("""
+kh-tc
+tc-ba
+bp-kh
+""");
     assert Graph.num_vertices(g) == 2
     assert Graph.num_edges(g) == 1
     assert Enum.sort(Graph.vertices(g)) == Enum.sort(["kh", "tc"])
