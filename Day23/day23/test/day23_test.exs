e77ede830eda8329235ac501e8ec3928327d5bc7
Chris Simon <chris@chrissimon.au>
2024-12-24 06:01:15 +1100

    5. RED - AoC Sample data for counting possible games

--- Day23/day23/test/day23_test.exs
+++ Day23/day23/test/day23_test.exs
@@ -19,4 +19,44 @@ bp-kh
     assert Graph.num_edges(g) == 3
     assert Enum.sort(Graph.vertices(g)) == Enum.sort(["kh", "tc", "ba", "bp"])
   end
+
+  test "finds number of game-sets with t" do
+    g = Day23.make_graph("""
+kh-tc
+qp-kh
+de-cg
+ka-co
+yn-aq
+qp-ub
+cg-tb
+vc-aq
+tb-ka
+wh-tc
+yn-cg
+kh-ub
+ta-co
+de-co
+tc-td
+tb-wq
+wh-td
+ta-ka
+td-qp
+aq-cg
+wq-ub
+ub-vc
+de-ta
+wq-aq
+wq-vc
+wh-yn
+ka-de
+kh-ta
+co-tc
+wh-qp
+tb-vc
+td-yn
+""")
+    assert Graph.num_vertices(g) == 16
+    assert Graph.num_edges(g) == 32
+    assert Day23.num_gamesets(g, "t") == 7
+  end
 end
