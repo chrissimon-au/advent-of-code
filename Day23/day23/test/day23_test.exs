9ddffd95365c0ffdbb3a15a3e5b2a6a47c7c96e3
Chris Simon <chris@chrissimon.au>
2024-12-24 00:14:45 +1100

    3. RED - ensure graph is parsing input for single connection

--- Day23/day23/test/day23_test.exs
+++ Day23/day23/test/day23_test.exs
@@ -6,5 +6,6 @@ defmodule Day23Test do
     g = Day23.make_graph("kh-tc");
     assert Graph.num_vertices(g) == 2
     assert Graph.num_edges(g) == 1
+    assert Enum.sort(Graph.vertices(g)) == Enum.sort(["kh", "tc"])
   end
 end
