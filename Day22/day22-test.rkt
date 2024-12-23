5415a04c1fbf62612188c44a95a81a9cd9dfb6a9
Chris Simon <chris@chrissimon.au>
2024-12-23 17:13:01 +1100

    4. RED - add more 'next secret' tests, one of which is failing

--- Day22/day22-test.rkt
+++ Day22/day22-test.rkt
@@ -8,3 +8,12 @@
 (check-eq? (prune 100000000) 16113920)
 
 (check-eq? (next-secret 123) 15887950)
+(check-eq? (next-secret 15887950) 16495136)
+(check-eq? (next-secret 16495136) 527345)
+(check-eq? (next-secret 527345) 704524)
+(check-eq? (next-secret 704524) 1553684)
+(check-eq? (next-secret 1553684) 12683156)
+(check-eq? (next-secret 12683156) 11100544)
+(check-eq? (next-secret 11100544) 12249484)
+(check-eq? (next-secret 12249484) 7753432)
+(check-eq? (next-secret 7753432) 5908254)
