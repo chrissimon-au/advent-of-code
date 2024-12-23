3876b84c43b6b2cbf143fdb50677565b21957f1e
Chris Simon <chris@chrissimon.au>
2024-12-23 17:07:02 +1100

    3. GREEN - next-secret-computation

--- Day22/day22.rkt
+++ Day22/day22.rkt
@@ -6,4 +6,29 @@
 (define (prune secret)
   (bitwise-and secret (- 16777215 1)))
 
-(provide mix prune)
+
+(define (step1 secret)
+  (prune
+   (mix secret
+        (arithmetic-shift secret 6)
+        )
+   ))
+
+(define (step2 secret)
+  (prune
+   (mix secret
+        (arithmetic-shift secret -5)
+        )
+   ))
+
+(define (step3 secret)
+  (prune
+   (mix secret
+        (arithmetic-shift secret 11)
+        )
+   ))
+
+(define (next-secret secret)
+  (step3 (step2 (step1 secret))))
+
+(provide mix prune next-secret)
