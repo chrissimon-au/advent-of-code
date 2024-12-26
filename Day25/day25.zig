3296c31914c6bc8e4447ce98447175db79fa31cd
Chris Simon <chris@devcycles.io>
2024-12-27 08:43:37 +1100

    4. REFACTOR - switch to test memory allocator and clean up deinits

--- Day25/day25.zig
+++ Day25/day25.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 const ArrayList = std.ArrayList;
+const Allocator = std.mem.Allocator;
 const expectEqual = std.testing.expectEqual;
 
 const KeyOrLock = union(enum) { key: Key, lock: Lock, neither };
@@ -21,11 +22,10 @@ fn parse_lock(input: []const u8) Lock {
     return lock;
 }
 
-fn parse_key(input: []const u8) Key {
+fn parse_key(input: []const u8, allocator: Allocator) Key {
     var row_input = std.mem.split(u8, input, "\n");
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    const allocator = gpa.allocator();
     var rows = ArrayList([]const u8).init(allocator);
+    defer rows.deinit();
     while (row_input.next()) |row| {
         rows.append(row) catch {};
     }
@@ -42,21 +42,19 @@ fn parse_key(input: []const u8) Key {
     return key;
 }
 
-fn parse_either(input: []const u8) KeyOrLock {
+fn parse_either(input: []const u8, allocator: Allocator) KeyOrLock {
     return switch (input[0]) {
-        '.' => KeyOrLock{ .key = parse_key(input) },
+        '.' => KeyOrLock{ .key = parse_key(input, allocator) },
         '#' => KeyOrLock{ .lock = parse_lock(input) },
         else => KeyOrLock.neither,
     };
 }
 
-fn parse_input(input: []const u8) ArrayList(KeyOrLock) {
+fn parse_input(input: []const u8, allocator: Allocator) ArrayList(KeyOrLock) {
     var items = std.mem.split(u8, input, "\n\n");
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    const allocator = gpa.allocator();
     var parsed_items = ArrayList(KeyOrLock).init(allocator);
     while (items.next()) |item| {
-        parsed_items.append(parse_either(item)) catch {};
+        parsed_items.append(parse_either(item, std.testing.allocator)) catch {};
     }
     return parsed_items;
 }
@@ -89,7 +87,7 @@ test parse_key {
         \\####.
         \\#####
     ;
-    const key: Key = parse_key(key_input);
+    const key: Key = parse_key(key_input, std.testing.allocator);
     try expectEqual(4, key.heights[0]);
     try expectEqual(3, key.heights[1]);
     try expectEqual(2, key.heights[2]);
@@ -107,7 +105,7 @@ test "parse either as key" {
         \\####.
         \\#####
     ;
-    const key = parse_either(key_input).key;
+    const key = parse_either(key_input, std.testing.allocator).key;
     try expectEqual(5, key.heights[0]);
     try expectEqual(3, key.heights[1]);
     try expectEqual(4, key.heights[2]);
@@ -125,7 +123,7 @@ test "parse either as lock" {
         \\..#..
         \\.....
     ;
-    const lock = parse_either(lock_input).lock;
+    const lock = parse_either(lock_input, std.testing.allocator).lock;
     try expectEqual(0, lock.pins[0]);
     try expectEqual(2, lock.pins[1]);
     try expectEqual(5, lock.pins[2]);
@@ -151,7 +149,8 @@ test parse_input {
         \\####.
         \\#####
     ;
-    const items = parse_input(input);
+    const items = parse_input(input, std.testing.allocator);
+    defer items.deinit();
     const lock = items.items[0].lock;
     try expectEqual(0, lock.pins[0]);
     try expectEqual(2, lock.pins[1]);
@@ -165,3 +164,24 @@ test parse_input {
     try expectEqual(1, key.heights[3]);
     try expectEqual(0, key.heights[4]);
 }
+
+// // From https://ziggit.dev/t/the-zig-way-to-read-a-file/4663/4
+// fn load_file(file_name: [] const u8) {
+//     const file = std.fs.cwd().openFile(file_name, .{}) catch |err| {
+//         std.log.err("Failed to open file: {s}", .{@errorName(err)});
+//         return;
+//     };
+//     defer file.close();
+
+//     while(file.reader().readUntilDelimiterOrEofAlloc(allocator, '\n', std.math.maxInt(usize)) catch |err| {
+//         std.log.err("Failed to read line: {s}", .{@errorName(err)});
+//         return;
+//     }) |line| {
+//         defer allocator.free(line);
+//     _ = line; // Do something with the line
+//     }
+// }
+
+// test "can parse_sample_input" {
+
+// }
