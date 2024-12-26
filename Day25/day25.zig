20781ef4e99d60b9538b8c1de281d86c961f2328
Chris Simon <chris@devcycles.io>
2024-12-27 07:21:59 +1100

    2. GREEN - can parse a key

--- Day25/day25.zig
+++ Day25/day25.zig
@@ -16,6 +16,27 @@ fn parse_lock(input: []const u8) [5]u8 {
     return lock;
 }
 
+fn parse_key(input: []const u8) [5]u8 {
+    var row_input = std.mem.split(u8, input, "\n");
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+    var rows = ArrayList([]const u8).init(allocator);
+    while (row_input.next()) |row| {
+        rows.append(row) catch {};
+    }
+    var lock = [5]u8{ 0, 0, 0, 0, 0 };
+    comptime var rowIdx = 5;
+    inline while (rowIdx > 0) : (rowIdx -= 1) {
+        const row = rows.items[rowIdx];
+        for (row, 0..) |pin, i| {
+            if (pin == '#') {
+                lock[i] += 1;
+            }
+        }
+    }
+    return lock;
+}
+
 test parse_lock {
     const lock =
         \\#####
@@ -32,4 +53,22 @@ test parse_lock {
     try expectEqual(2, lock_pins[2]);
     try expectEqual(3, lock_pins[3]);
     try expectEqual(4, lock_pins[4]);
+}
+
+test parse_key {
+    const key =
+        \\.....
+        \\.....
+        \\#....
+        \\##...
+        \\###..
+        \\####.
+        \\#####
+    ;
+    const key_heights = parse_key(key);
+    try expectEqual(4, key_heights[0]);
+    try expectEqual(3, key_heights[1]);
+    try expectEqual(2, key_heights[2]);
+    try expectEqual(1, key_heights[3]);
+    try expectEqual(0, key_heights[4]);
 }
