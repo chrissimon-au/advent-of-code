7c431bce1b2e6fd6b61defd0d064ae3a6755fab9
Chris Simon <chris@chrissimon.au>
2024-12-29 10:06:13 +1100

    Add day 2 in prep for day2

---
+++ 2015/day02/day02.rkt
@@ -0,0 +1,25 @@
+#lang typed/racket
+
+(require
+  typed/rackunit
+  typed/rackunit/text-ui
+  "../common.rkt")
+
+
+
+
+(define (part1 [_input : String]) : Number 0)
+(define (part2 [_input : String]) : Number 0)
+
+(module+
+    test
+
+  (run-tests
+   (test-suite
+    "day-02"
+    ;(check-aoc part1 "sample" "1")
+    ;(check-aoc part2 "sample" "2")
+    ;(check-aoc part1 "test" "1")
+    ;(check-aoc part2 "test" "2")
+    ))
+  )
