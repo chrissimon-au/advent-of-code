d2bb5baca4e73eb3c47bd2accbfe73b0d247a6f8
Chris Simon <chris@chrissimon.au>
2024-12-28 21:28:53 +1100

    2015 Day 1 part 2

--- 2015/day01.rkt
+++ 2015/day01.rkt
@@ -14,6 +14,30 @@
   (- (string-count instructions #\() (string-count instructions #\)))
   )
 
+(define
+  (apply-instruction [current-floor : Number] [instruction : Char]) : Number
+  (if (eq? instruction #\()
+      (+ current-floor 1)
+      (- current-floor 1))
+  )
+
+(define
+  (position-of-floor-calc [instructions : (Listof Char)] [target-floor : Number] [current-floor : Number] [instruction-counter : Number]) : Number
+  (if (eq? current-floor target-floor)
+      instruction-counter
+      (position-of-floor-calc
+       (cdr instructions)
+       target-floor
+       (apply-instruction current-floor (car instructions))
+       (+ instruction-counter 1)
+       ))
+  )
+
+(define
+  (position-of-floor [instructions : String] [target-floor : Number]) : Number
+  (position-of-floor-calc (string->list instructions) target-floor 0 0)
+  )
+
 (module+ test
 
   (define (file-contents [file-name : Path-String]) : String
@@ -28,6 +52,7 @@
      (check-equal? (final-floor "()()") 0)
      (check-equal? (final-floor "(((") 3)
      (check-equal? (final-floor (file-contents "day01.test.data")) (string->number (file-contents "day01.test-answer.data")))
+     (check-equal? (position-of-floor (file-contents "day01.test.data") -1) (string->number (file-contents "day01.test-answer2.data")))
      ))
   (run-tests day01)
   )
