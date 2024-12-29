2a062f01d0caaf8f9b94b84bccf6f40a7dc83112
Chris Simon <chris@chrissimon.au>
2024-12-29 21:22:35 +1100

    Update new-day to reflect new usage of data files

--- 2015/new-day.sh
+++ 2015/new-day.sh
@@ -2,10 +2,10 @@ mkdir -p day$1
 pushd day$1
 cp ../day-template/dayDD.rkt day$1.rkt
 touch test.data
-touch test-answer.data
+touch test-answer1.data
 touch test-answer2.data
 touch sample.data
-touch sample-answer.data
+touch sample-answer1.data
 touch sample-answer2.data
 sed -i 's/DD/'"$1"'/g' day$1.rkt
 popd
