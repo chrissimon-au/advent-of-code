43dd44237614201b8a6532cc3b52aacd94885ca5
Chris Simon <chris@chrissimon.au>
2024-12-30 07:34:25 +1100

    Day 03 part 2

--- 2015/day03/day03.rkt
+++ 2015/day03/day03.rkt
@@ -30,17 +30,39 @@
     (SantaState new-pos (set-add (SantaState-visited-locations state) new-pos))
     ))
 
+(define
+  (follow-instructions [instructions : (Listof Char)])
+  (foldl update-santa-state (initial-santa-state) instructions))
 
 (define
   (num-houses [input : String]) : Number
   (let* (
          [instructions (string->list input)]
-         [final-state (foldl update-santa-state (initial-santa-state) instructions)]
+         [final-state (follow-instructions instructions)]
          )
     (set-count (SantaState-visited-locations final-state))))
 
+
+(: alternating-elements (All (A) (-> (Listof A) (Listof A))))
+(define (alternating-elements lst)
+  (cond ((null? lst) '())
+        ((null? (cdr lst)) (list (car lst)))
+        (else (cons (car lst) (alternating-elements (cddr lst))))))
+
+(define
+  (num-houses-with-robo-santa [input : String]) : Number
+  (let* (
+         [instructions (string->list input)]
+         [santa-instructions (alternating-elements instructions)]
+         [robosanta-instructions (alternating-elements (cdr instructions))]
+         [santa-final-state (follow-instructions santa-instructions)]
+         [robosanta-final-state (follow-instructions robosanta-instructions)]
+         [all-locations (set-union (SantaState-visited-locations santa-final-state) (SantaState-visited-locations robosanta-final-state))]
+         )
+    (set-count all-locations)))
+
 (define (part1 [input : String]) : Number (num-houses input))
-(define (part2 [_input : String]) : Number 0)
+(define (part2 [input : String]) : Number (num-houses-with-robo-santa input))
 
 (module+
     test
@@ -51,9 +73,11 @@
     (check-equal? (num-houses ">") 2)
     (check-equal? (num-houses ">^") 3)
     (check-equal? (num-houses ">^v") 3)
+    (check-equal? (num-houses-with-robo-santa "^v") 3)
+    (check-equal? (num-houses-with-robo-santa "^v^v^v^v^v") 11)
     ;(check-aoc part1 "sample" "1")
     ;(check-aoc part2 "sample" "2")
     (check-aoc part1 "test" "1")
-    ;(check-aoc part2 "test" "2")
+    (check-aoc part2 "test" "2")
     ))
   )
