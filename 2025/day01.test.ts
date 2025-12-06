c0f35174882b7c3ac44c4c483738e7fe07f12ae1
Chris Simon <chris@chrissimon.au>
2025-12-06 14:04:42 +0000

    clean up

--- 2025/day01.test.ts
+++ 2025/day01.test.ts
@@ -1,9 +1,9 @@
 import {test,expect} from 'vitest'
 import {part1,part2} from './day01.ts'
 import {readFileSync} from 'fs';
-//test("none", () => expect(part1("R1")).toBe(0));
-//test("one", () => expect(part1("L25\nL25")).toBe(1));
-//test("right", () => expect(part1("R25\nL75")).toBe(1));
+test("none", () => expect(part1("R1")).toBe(0));
+test("one", () => expect(part1("L25\nL25")).toBe(1));
+test("right", () => expect(part1("R25\nL75")).toBe(1));
 const sample = `L68
 L30
 R48
@@ -17,8 +17,10 @@ L82`;
 test("sample", () => expect(part1(sample)).toBe(3));
 
 
-const t1=readFileSync("testdata.day01.part1.txt").toString("UTF-8");
-const t1a=parseInt(readFileSync("testdata.day01.part1.answer.txt").toString("UTF-8"));
+const t1=readFileSync("testdata.day01.part1.txt").toString();
+const t1a=parseInt(readFileSync("testdata.day01.part1.answer.txt").toString());
+
+test("part1", () => expect(part1(t1)).toBe(t1a));
 
 test("p2 simple", () => expect(part2("R25")).toBe(0));
 test("p2 1 rotate", ()=> expect(part2("R60")).toBe(1));
@@ -30,5 +32,5 @@ test("p2 c1", () => expect(part2("L199")).toBe(2));
 
 
 test("sample2", () => expect(part2(sample)).toBe(6));
-
-test("part2", () => expect(part2(t1)).toBe(0));
+const t2a = parseInt(readFileSync("testdata.day01.part2.answer.txt").toString())
+test("part2", () => expect(part2(t1)).toBe(t2a));
--- 2025/day01.ts
+++ 2025/day01.ts
@@ -16,7 +16,7 @@ export function part2(input: string) {
     var pos=50;
     var counter=0;
     const lines=input.split("\n");
-    console.log("pos", pos);
+    //console.log("pos", pos);
     for (const l of lines) {
         const dir=l[0];
         const num=parseInt(l.substring(1));
@@ -28,13 +28,13 @@ export function part2(input: string) {
         const numRotations=Math.round((starting+num-50)/100);
         
         
-        console.log("num",l,"starting", starting,"rotation:",starting+num-50);
+        //console.log("num",l,"starting", starting,"rotation:",starting+num-50);
 
         pos=(newPos+1000000000) %100;
         const atZero = numRotations == 0 && num > 0 && pos == 0? 1: 0;
         
         counter=counter+numRotations + atZero;
-        console.log("pos",pos,"counter",counter,"numRotations",numRotations, "atZero", atZero)
+        //console.log("pos",pos,"counter",counter,"numRotations",numRotations, "atZero", atZero)
     }
     return counter;
 }
