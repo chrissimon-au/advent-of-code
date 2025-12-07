135325ade421186e848312b18d4f7f205d964595
Chris Simon <chris@chrissimon.au>
2025-12-07 20:35:19 +0100

    2025 day02 part 2 done

--- 2025/day02.test.ts
+++ 2025/day02.test.ts
@@ -1,32 +1,32 @@
-import {describe as d, test as t,expect as e} from'vitest';
+import { describe as d, test as t, expect as e } from 'vitest';
 import { readFileSync } from 'fs';
 
-import { part1 as p1, part2 as p2} from './day02.ts'
+import { part1 as p1, part2 as p2 } from './day02.ts'
 
 const t1 = readFileSync("day02.p1.testdata.txt").toString();
-const sample="11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124";
+const sample = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124";
 
-d("part1",()=> {
-t("no bad ids", () => e(p1("1698522-169852")).toBe(0));
-t("small range ids", () => e(p1("446443-446449")).toBe(446446));
-t("diff digits", () => e(p1("998-1012")).toBe(1010));
-t("multiple ids", () => e(p1("11-22")).toBe(33));
-t("multiple ranges", () => e(p1("11-22,95-115")).toBe(132));
-t("first below start", () => e(p1("4959-5051")).toBe(5050));
-t("sample", () => e(p1(sample)).toBe(1227775554));
-const t1a = parseInt(readFileSync("day02.p1.answer.txt").toString());
-t("p1", () => e(p1(t1)).toBe(t1a));
+d("part1", () => {
+    t("no bad ids", () => e(p1("1698522-169852")).toBe(0));
+    t("small range ids", () => e(p1("446443-446449")).toBe(446446));
+    t("diff digits", () => e(p1("998-1012")).toBe(1010));
+    t("multiple ids", () => e(p1("11-22")).toBe(33));
+    t("multiple ranges", () => e(p1("11-22,95-115")).toBe(132));
+    t("first below start", () => e(p1("4959-5051")).toBe(5050));
+    t("sample", () => e(p1(sample)).toBe(1227775554));
+    const t1a = parseInt(readFileSync("day02.p1.answer.txt").toString());
+    t("p1", () => e(p1(t1)).toBe(t1a));
 });
 
-d("part2",()=>{
-t("tripleRepeat", ()=>e(p2("121211-121213")).toBe(121212));
-t("1", ()=>e(p2("11-22")).toBe(33));
-t("2", ()=>e(p2("95-115")).toBe(210));
-t("3", ()=>e(p2("998-1012")).toBe(2009));
-t("4", ()=>e(p2("121211-121213")).toBe(121212));
-t("first below start", () => e(p2("495900-505500")).toBe(5009550));
-t("sample", () => e(p2(sample)).toBe(4174379265));
-const t2a = parseInt(readFileSync("day02.p2.answer.txt").toString());
+d("part2", () => {
+    t("tripleRepeat", () => e(p2("121211-121213")).toBe(121212));
+    t("1", () => e(p2("11-22")).toBe(33));
+    t("2", () => e(p2("95-115")).toBe(210));
+    t("3", () => e(p2("998-1012")).toBe(2009));
+    t("4", () => e(p2("121211-121213")).toBe(121212));
+    t("first below start", () => e(p2("495900-505500")).toBe(5009550));
+    t("sample", () => e(p2(sample)).toBe(4174379265));
+    const t2a = parseInt(readFileSync("day02.p2.answer.txt").toString());
 
-t("p2", () => e(p2(t1)).toBe(t2a));
+    t("p2", () => e(p2(t1)).toBe(t2a));
 });
--- 2025/day02.ts
+++ 2025/day02.ts
@@ -1,81 +1,85 @@
-export function part1(input:string) {
-  const ranges=input.split(",");
-  var acc=0;
+export function part1(input: string) {
+  const ranges = input.split(",");
+  var acc = 0;
   for (const range of ranges) {
 
-  const [start,end] = range.split('-');
-  const startN = parseInt(start);
-  const endN = parseInt(end);
+    const [start, end] = range.split('-');
+    const startN = parseInt(start);
+    const endN = parseInt(end);
 
-  const halfLength = Math.floor(start.length/2);
+    const halfLength = Math.floor(start.length / 2);
 
-  const startHalf = start.length % 2 == 0 ? start.substring(0,halfLength) : "1" + "0".repeat(halfLength);
-  
-  const startHN = parseInt(startHalf);
+    const startHalf = start.length % 2 == 0 ? start.substring(0, halfLength) : "1" + "0".repeat(halfLength);
 
-  var t = startHN;
-  var i = parseInt(startHalf+startHalf);
-  console.log("startHalf", startHalf)
-  while (i<startN) {
-    t++;
-    const tStr = t.toString();
-    i = parseInt(tStr+tStr);
+    const startHN = parseInt(startHalf);
+
+    var t = startHN;
+    var i = parseInt(startHalf + startHalf);
+    console.log("startHalf", startHalf)
+    while (i < startN) {
+      t++;
+      const tStr = t.toString();
+      i = parseInt(tStr + tStr);
+    }
+    while (i <= endN && i >= startN) {
+      acc += i;
+      t++;
+      const tStr = t.toString();
+      i = parseInt(tStr + tStr);
+    }
   }
-  while (i<=endN && i>=startN) {
-    acc+=i;
-    t++;
-    const tStr = t.toString();
-    i = parseInt(tStr+tStr);
-  }
-}
 
   return acc;
 }
 
 export function part2(input: string) {
-  const ranges=input.split(",");
-  var acc=0;
+  const ranges = input.split(",");
+  var acc = 0;
   for (const range of ranges) {
     const counted = new Set();
-    const [start,end] = range.split('-');
+    const [start, end] = range.split('-');
     const startN = parseInt(start);
     const endN = parseInt(end);
-    const maxChunkSize=Math.floor(Math.max(start.length,end.length)/2);
-    for (var chunkSize=1;chunkSize<=maxChunkSize;chunkSize++) {
-      var numChunks=Math.floor(start.length/chunkSize);
-      var firstChunk = start.substring(0,chunkSize);
-      var s = parseInt(firstChunk);
+    const maxChunkSize = Math.floor(Math.max(start.length, end.length) / 2);
+    console.log("===========================");
+    console.log("start", start, "end", end, "maxChunkSize", maxChunkSize);
+    for (var chunkSize = 1; chunkSize <= maxChunkSize; chunkSize++) {
+      var numChunks = Math.floor(start.length / chunkSize);
+      var firstChunk = start.substring(0, chunkSize);
       if (numChunks == 1) {
         numChunks++;
-        firstChunk = "1" + "0".repeat(chunkSize-1);
+        firstChunk = "1" + "0".repeat(chunkSize - 1);
       }
+      var s = parseInt(firstChunk);
       const t = firstChunk.repeat(numChunks);
       var i = parseInt(t);
-      console.log("chunkSize", chunkSize, "numChunks", numChunks, "s", s, "t", t)
+      console.log("   ", "chunkSize", chunkSize, "numChunks", numChunks, "s", s, "t", t)
       while (i < startN) {
         s++;
         const sStr = s.toString();
         const t = sStr.repeat(numChunks);
         i = parseInt(t);
       }
-      console.log("b" ,s, i, startN, endN);
+      console.log("    ", "starting to accumulate", "s", s, "i", i);
       while (i >= startN && i <= endN) {
         if (!counted.has(i)) {
           acc += i;
           counted.add(i);
           console.log("accumulating", i);
+        } else {
+          console.log(" not accumulating, already counted", i);
         }
         s++;
         var sStr = s.toString();
         if (sStr.length > chunkSize) {
-          sStr = "1" + "0".repeat(chunkSize-1);
+          sStr = "1" + "0".repeat(chunkSize - 1);
           s = parseInt(sStr);
           numChunks++;
-          console.log("  ", "new numChunks",numChunks,sStr)
+          console.log("  ", "new numChunks", numChunks, sStr)
         }
         const t = sStr.repeat(numChunks);
-        i = parseInt(t); 
-        console.log(s,i);
+        i = parseInt(t);
+        console.log("       ", "s", s, "i", i);
       }
     }
 
