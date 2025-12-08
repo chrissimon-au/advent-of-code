5889e4f2e220cdcbe06879dcd617b91a487783cb
Chris Simon <chris@chrissimon.au>
2025-12-08 14:43:31 +0100

    2025 day07 part 2 done

--- 2025/day07.ts
+++ 2025/day07.ts
@@ -2,11 +2,15 @@ function isSplitter(line: string, pos: number): boolean {
     return line[pos] == '^';
 }
 
-export function part1(input: string) {
+function getManifold(input: string) {
     const lines = input.split('\n');
-    const manifold = lines.filter((_, i) => i % 2 == 0);
+    return lines.filter((_, i) => i % 2 == 0);
+}
+
+export function part1(input: string) {
+    const manifold = getManifold(input);
     var tachyons = [manifold[0].indexOf('S')]
-    var totalSplits = 0;    
+    var totalSplits = 0;
     for (var y = 1; y < manifold.length; y++) {
         const manifoldRow = manifold[y];
         const numSplits = tachyons.reduce((acc, tachyonPos) => acc + (isSplitter(manifoldRow, tachyonPos) ? 1 : 0), 0)
@@ -16,6 +20,32 @@ export function part1(input: string) {
     return totalSplits;
 }
 
-export function part2(input: string) {
 
+export function part2(input: string) {
+    const manifold = getManifold(input);
+    var tachyons: number[] = []
+    tachyons[manifold[0].indexOf('S')] = 1;
+    console.log(tachyons);
+    var totalTimelines = 0;
+    for (var y = 1; y < manifold.length; y++) {
+        const manifoldRow = manifold[y];
+        const newTachyons = [];
+        tachyons.forEach((count, tachyonPos) => {
+            if (isSplitter(manifoldRow, tachyonPos)) {
+                newTachyons[tachyonPos - 1] = (newTachyons[tachyonPos - 1] ?? 0) + count;
+                newTachyons[tachyonPos + 1] = (newTachyons[tachyonPos + 1] ?? 0) + count;
+            } else {
+                newTachyons[tachyonPos] = (newTachyons[tachyonPos] ?? 0) + count;
+            }
+        });
+        tachyons = newTachyons;
+        // console.log(manifoldRow);
+        // let tachyonRow = Array(manifoldRow.length)
+        // tachyons.forEach((c, i) => tachyonRow[i] = c.toString());
+        // tachyonRow = [...tachyonRow].map((c, i) => c ? c : ".");
+        // console.log(tachyonRow.join(''))
+        //console.log("new tachyons", tachyons);
+        //console.log("num timelines", tachyons.reduce((acc, i) => acc + i, 0));
+    }
+    return tachyons.reduce((acc, i) => acc + i, 0);
 }
