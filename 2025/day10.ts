08cca4581b4e46626071ac3b6abb8e2cef9c96ac
Chris Simon <chris@chrissimon.au>
2025-12-10 22:29:01 +0100

    2025 day 10 part 2 wip - linear regression approach but failing to find integer positive solutions

--- 2025/day10.ts
+++ 2025/day10.ts
@@ -9,13 +9,20 @@ import {
     vec,
     prettyPrint,
     solve,
+    ones,
+    euclideanNorm,
     Vector,
+    Matrix,
     FloatMatrix,
     calculateLinearLeastSquares,
     calculateGeneralLeastSquares,
     calculateSingularValueDecomposition,
     UnderdeterminedSolution,
-    ApproximationFunctionTemplate
+    ApproximationFunctionTemplate,
+    LinearSolution,
+    SolutionType,
+    UniqueSolution,
+    FloatVector
 } from '@josh-brown/vector';
 
 type Button = number[];
@@ -93,57 +100,150 @@ function buttonToVector(button: Button, length: number): number[] {
     return buttonVec;
 }
 
-function vectorCost(v: Vector) {
-    return v.toArray().reduce((acc, v) => acc + v);
+function isValidSlnVector(v: Vector<number>) {
+    return v.toArray().every(a => Math.round(a) >= 0 && Math.abs(Math.round(a) - a) < Number.EPSILON);
+}
+
+function isValid(sln: LinearSolution<number>) {
+    if (sln.solutionType == SolutionType.OVERDETERMINED) {
+        return false;
+    }
+    return isValidSlnVector(sln.solution);
+}
+
+function slnCost(v: Vector<number>) {
+    return v.toArray().reduce((a, i) => a + i);
+}
+
+function slnError(v: Vector<number>) {
+    return -(v.toArray().filter(a => a < 0).reduce((a, i) => a + i, 0));
+}
+
+function logVecArr(name: string, m: Matrix<number> | Vector<number>) {
+    console.log(name);
+    const mp = m instanceof FloatMatrix ?
+        prettyPrint(m)
+        : (m instanceof FloatVector ? prettyPrint(m) : `${m.toArray()}`);
+    console.log(mp);
 }
 
 const matrixBuilder = FloatMatrix.builder()
+const vectorBuilder = FloatVector.builder()
 
-function getPressesToTargetJoltage(machine: Machine): number {
+function findSolutionAtCost(cost: number, targetJoltages: JoltageLevels, verificationMatrix: FloatMatrix, log: boolean): LinearSolution<number> {
+    if (log) { console.log("-----------"); }
+    if (log) { console.log("Target cost", cost); }
+    const verificationTarget = vec([...targetJoltages, cost]);
+    if (log) { logVecArr("Verification Target at cost", verificationTarget); }
 
-    const buttonMatrix = mat(machine.buttons.map(b => buttonToVector(b, machine.targetJoltageLevels.length))).transpose();
-    console.log("buttons:");
-    console.log(prettyPrint(buttonMatrix));
 
-    const targetJoltages = vec(machine.targetJoltageLevels);
-    console.log("Target:");
-    console.log(prettyPrint(targetJoltages));
+    var sln: LinearSolution<number> = null;
+    try {
+        sln = solve(verificationMatrix, verificationTarget);
+    } catch (ex) {
+        if (log) { console.log("Error attempting solve", ex) }
+        return { solutionType: SolutionType.OVERDETERMINED };
+    }
 
-    const initialSln = solve(buttonMatrix, targetJoltages) as UnderdeterminedSolution<number>;
-    console.log("initial solution");
-    console.log(prettyPrint(initialSln.solution));
+    if (isValid(sln)) {
+        if (log) { logVecArr("Found Sln on first attempt", (sln as UnderdeterminedSolution<number>).solution) }
+        return sln;
+    }
+    if (sln.solutionType == SolutionType.OVERDETERMINED) {
+        if (log) { console.log("Overdetermined, abandoning"); }
+        return sln;
+    }
 
-    const columns = buttonMatrix.getColumnVectors();
-    const linearData = matrixBuilder.fromColumnVectors([...columns, targetJoltages]).getRowVectors();
-    //const res = calculateLinearLeastSquares(linearData.getRowVectors());
+    if (log) {
+        logVecArr("Not valid first attempt", sln.solution);
+    }
+    const dims = sln.solution.getDimension();
 
-    const ops = linearData[0].ops();
-    const linearFunctionTemplate: ApproximationFunctionTemplate<number> = (coefficients) => {
-        console.log(" new coefficients:")
-        console.log(prettyPrint(coefficients));
-        return (input: Vector<number>) => {
-            console.log("   new input");
-            console.log(prettyPrint(input));
-            let value = 0; // constant term
-            for (let i = 0; i < coefficients.getDimension(); i++) {
-                console.log(coefficients.getEntry(i), input.getEntry(i))
-                const newTerm = ops.multiply(coefficients.getEntry(i), input.getEntry(i));
-                value = ops.add(value, newTerm);
-            }
-            return value;
-        };
-    };
+    const zeroPositions = sln.solution.toArray().map((z, i) => [z, i]).filter(([z, i]) => z < 0).map(([z, i]) => i);
+    const zeroRows = zeroPositions.map(zp => vectorBuilder.fromIndexFunction(dims, (i) => zp == i ? 1 : 0));
+    const rows = verificationMatrix.getRowVectors();
+    const verifyWithZerosMatrix = matrixBuilder.fromRowVectors([...rows, ...zeroRows]);
+    const zeroVerificationTarget = vec([...verificationTarget.toArray(), ...Array(zeroPositions.length).fill(0)])
 
-    const res = calculateGeneralLeastSquares(linearData, linearFunctionTemplate, linearData[0].getDimension() - 1);
-    console.log("linear regression solution");
-    console.log(prettyPrint(res.coefficients));
-    let args = res.coefficients.toArray().map(x => Math.round(x))
-    console.log("  after rounding");
-    console.log(args);
-    return args.reduce((acc, i) => acc + i);
+    if (log) {
+        logVecArr("VerifywithZerosMatrix", verifyWithZerosMatrix);
+        logVecArr("ZeroVerificationTarget", zeroVerificationTarget);
+    }
+
+    try {
+        const origSln = sln;
+        sln = solve(verifyWithZerosMatrix, zeroVerificationTarget);
+        if (log) { console.log("Solution found after attempt with zeroes", sln) }
+        if (sln.solutionType == SolutionType.OVERDETERMINED) {
+            return origSln;
+        }
+        return sln;
+    } catch (ex) {
+        if (log) { console.log("Error attempting solve", ex) }
+        return sln;
+    }
 }
 
-function getButtonPressesForJoltageLevels(line: string) {
+function getPressesToTargetJoltage(machine: Machine): number {
+    const logAll = false;
+
+    const buttonMatrix = mat(machine.buttons.map(b => buttonToVector(b, machine.targetJoltageLevels.length))).transpose();
+    if (logAll) { logVecArr("buttons", buttonMatrix); }
+
+    const targetJoltages = vec(machine.targetJoltageLevels);
+    if (logAll) { logVecArr("Target:", targetJoltages); }
+
+    const verificationRow = ones(machine.buttons.length);
+    const rows = buttonMatrix.getRowVectors();
+    const verificationMatrix = matrixBuilder.fromRowVectors([...rows, verificationRow]);
+
+    // minimum presses is the largest target joltage
+    var cost = Math.max(...machine.targetJoltageLevels);
+    // maximum presses is the sum of all target joltages
+    const MaxCost = machine.targetJoltageLevels.reduce((a, i) => a + i);
+    // solution must be in this range.
+
+    const underDeterminedSolutions: (UnderdeterminedSolution<number> | UniqueSolution<number>)[] = [];
+
+    var sln: LinearSolution<number> = findSolutionAtCost(cost, machine.targetJoltageLevels, verificationMatrix, logAll);
+
+    while (!isValid(sln)) {
+        const log = logAll;//iterator % 1000 == 0;
+        cost++;
+
+        if (cost > MaxCost) {
+            if (underDeterminedSolutions.length == 1) {
+                console.log();
+                logVecArr("Only one underdetermined solution found in range, it must be it", underDeterminedSolutions[0].solution);
+                return Math.round(slnCost(underDeterminedSolutions[0].solution));
+            }
+            if (underDeterminedSolutions.length > 1) {
+                const sortedUndeterminedCosts = underDeterminedSolutions.map(s => slnCost(s.solution)).sort();
+                //const sortedUndetermined = underDeterminedSolutions.sort((s1, s2) => slnError(s1.solution) - slnError(s2.solution));
+                //sortedUndetermined.forEach((s, i) => logVecArr(`undetermined ${i}`, s.solution));
+                //const lowestErrorUndetermined = sortedUndetermined[0];
+                //throw new Error("need to check handling of multiple undetermineds")
+                console.log("Selecting lowest cost from all underdetermined solutions", sortedUndeterminedCosts[0])
+                return sortedUndeterminedCosts[0];
+            }
+            throw new Error(`Cost ${cost} exceeds max cost ${MaxCost} without any underdetermined solutions`)
+        }
+
+        if (sln.solutionType == SolutionType.UNDERDETERMINED || sln.solutionType == SolutionType.UNIQUE) {
+            underDeterminedSolutions.push(sln);
+        }
+
+        sln = findSolutionAtCost(cost, machine.targetJoltageLevels, verificationMatrix, log);
+
+
+    }
+    logVecArr(`Found solution with cost ${cost}`, (sln as UnderdeterminedSolution<number>).solution);
+    return cost;
+}
+
+function getButtonPressesForJoltageLevels(line: string, idx: number) {
+    console.log(`${idx} ===============`);
+    console.log(line);
     const machine = parseMachine(line);
     //console.log(machine);
     return getPressesToTargetJoltage(machine);
@@ -156,5 +256,5 @@ export function part1(input: string) {
 
 export function part2(input: string) {
     const machineLines = input.split('\n');
-    return machineLines.reduce((acc, machineLine) => acc + getButtonPressesForJoltageLevels(machineLine), 0);
+    return machineLines.reduce((acc, machineLine, i) => acc + getButtonPressesForJoltageLevels(machineLine, i), 0);
 }
